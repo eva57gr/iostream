cefb596f3468d4519e1b990f8c671dc541ed5776
eva57gr <eva57gradycdc@hotmail.com>
2022-06-20 10:58:33 +0300

    Operational Task #4741
    Operational Task #4741

---
+++ main/_26_Quarter/List of Controls_TRIM.sql
@@ -0,0 +1,50 @@
+﻿SELECT 
+      L.CONTROL,
+      L.FORMULA,
+      L.PRIORITATEA,
+      L.STATUT
+      
+      
+        
+        FROM
+
+( 
+SELECT 
+      B.CONTROL,
+      B.FORMULA,
+      B.PRIORITATEA,
+      B.STATUT
+      
+      
+        
+        FROM CIS2.MD_CONTROL B  INNER JOIN (
+        SELECT 
+        A.CONTROL, 
+        MAX(A.CONTROL_VERS) CONTROL_VERS
+        
+        FROM CIS2.MD_CONTROL A
+        
+        
+         WHERE
+         1=1
+         AND A.FORM  = 26
+               
+         GROUP BY 
+          A.CONTROL
+         
+         ORDER BY 
+          A.CONTROL
+        
+        )  A  ON A.CONTROL = B.CONTROL AND A.CONTROL_VERS = B.CONTROL_VERS   
+        
+        
+         WHERE
+         1=1
+         AND B.FORM  = 26
+         AND B.STATUT <> '3'
+        -- AND B.FORM_VERS = 2009
+               
+         
+         
+         ORDER BY 
+          B.CONTROL ) L
