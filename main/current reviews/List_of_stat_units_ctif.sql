235ac20b6c085759fcd2b586a5dd89b3b4f8d088
eva57gr <eva57gradycdc@hotmail.com>
2022-06-29 09:38:01 +0300

    CNT_CTIF_.sql
    CNT_CTIF_.sql

--- main/current reviews/List_of_stat_units_ctif.sql
+++ main/current reviews/List_of_stat_units_ctif.sql
@@ -8,19 +8,19 @@
         D.FORM_VERS,
         D.ID_SCHEMA
         
-        FROM    USER_EREPORTING.VW_DATA_ALL_PRIMIT D
+        FROM    USER_EREPORTING.VW_DATA_ALL_FR_PRIMIT D
         
         WHERE 
-        D.ANUL = 2021 
+        D.PERIOADA =  2010 
 
         GROUP BY
         D.FORM,
         D.FORM_VERS,
         D.ID_SCHEMA
         
-        HAVING 
-        
-        D.FORM_VERS = 1004
+--        HAVING 
+--        
+--        D.FORM_VERS = 1004
         
         ORDER BY 
         D.ID_SCHEMA,
