7e90bfbb81a387d5932c35bedcd1932d765951d9
eva57gr <eva57gradycdc@hotmail.com>
2022-07-04 14:49:30 +0300

    Operational Task #4826 #57
    Operational Task #4826 #57

---
+++ main/ASA/SQL_M1.sql
@@ -0,0 +1,36 @@
+SELECT
+            D.ANUL, 
+            D.CUIIO,
+            D.CUATM, 
+            D.RIND,
+        
+            SUM(D.COL9) AS NSAL,
+            SUM(D.COL10) AS REM_MUNC
+
+
+              FROM CIS2.VW_DATA_ALL D
+                
+                        
+                
+                    WHERE 
+                    
+                    D.CAPITOL IN (1128)
+        
+                    AND D.FORM IN (64)
+                    AND D.PERIOADA = 2010   
+                   -- AND D.RIND  IN ('500')
+                    
+               --     AND D.CUIIO = 400053
+                    
+                    
+                    GROUP BY 
+                       D.ANUL, 
+            D.CUIIO,
+            D.CUATM, 
+            D.RIND
+            
+            ORDER BY 
+              D.CUATM, 
+              D.CUIIO,
+          
+            D.RIND
--- main/Add  new SU-s/Add  new SU-s_7.sql
+++ main/Add  new SU-s/Add  new SU-s_7.sql
@@ -1,4 +1,4 @@
-
+--
 --INSERT INTO CIS2.FORM_CUIIO R (
 --        CUIIO,
 --        CUIIO_VERS,
@@ -6,23 +6,40 @@
 --        FORM_VERS,
 --        STATUT 
 --)
-
+--
 
 
  SELECT 
  
         L.CUIIO,
         L.CUIIO_VERS,
-        43 FORM,
-        2000   FORM_VERS,
+        26 FORM,
+        1004   FORM_VERS,
         '1' STATUT
        
-        FROM USER_BANCU.ADD_NEW_SU L
+        FROM --USER_BANCU.ADD_NEW_SU L
+          RENIM L 
         
         WHERE 
         
-        L.CUIIO NOT IN (
-        41191673
-        ) 
-                      
-                                                  
+CUIIO IN (
+41428030,
+40966698,
+41446825,
+41509075,
+41479138,
+41509081,
+41560426,
+41267229,
+40874743,
+37107520,
+41154175,
+41191182,
+38500857
+
+) 
+
+
+ AND 
+ 
+ CUIIO_VERS IN (1053)                                
