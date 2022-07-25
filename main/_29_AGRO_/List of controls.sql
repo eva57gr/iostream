d928c9b7539772cfad9bc734e659a820a579fd65
eva57gr <eva57gradycdc@hotmail.com>
2022-07-25 09:58:33 +0300

    update

---
+++ main/_29_AGRO_/List of controls.sql
@@ -0,0 +1,81 @@
+SELECT 
+      L.CONTROL,
+      L.FORMULA,
+      L.SQL_TEXT,
+      L.PRIORITATEA,
+      L.STATUT
+      
+      
+        
+        FROM
+
+( 
+
+SELECT 
+      L.CONTROL,
+      L.FORMULA,
+      L.SQL_TEXT,
+      L.PRIORITATEA,
+      L.STATUT
+      
+      
+        
+        FROM
+
+( 
+SELECT 
+      B.CONTROL,
+      B.FORMULA,
+      B.SQL_TEXT,
+      B.PRIORITATEA,
+      B.STATUT
+      
+      
+        
+        FROM CIS2.MD_CONTROL B  INNER JOIN (
+        SELECT 
+        A.CONTROL, 
+        MAX(A.CONTROL_VERS) CONTROL_VERS
+        
+        FROM CIS2.MD_CONTROL A
+        
+        
+         WHERE
+         1=1
+         AND A.FORM  = 45
+         
+         AND A.CONTROL_VERS <=:pPERIOADA 
+               
+         GROUP BY 
+          A.CONTROL
+         
+         ORDER BY 
+          A.CONTROL
+        
+        )  A  ON A.CONTROL = B.CONTROL AND A.CONTROL_VERS = B.CONTROL_VERS   
+        
+        
+         WHERE
+         1=1
+         AND B.FORM  = 45
+         AND B.STATUT <> '3'
+         
+        
+        
+               
+         
+         
+         ORDER BY 
+          B.CONTROL ) L
+          
+          WHERE 
+          1=1
+          AND L.FORMULA LIKE '%=%'
+          ) L
+          
+            WHERE 
+         1=1  
+          
+         AND  L.FORMULA  NOT LIKE '%>=%'
+          AND 
+          L.FORMULA  NOT LIKE '%=>%'
