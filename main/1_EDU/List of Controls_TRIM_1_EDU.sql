d2adbb20c678f65062a417f42397f4c676442ca0
eva57gr <eva57gradycdc@hotmail.com>
2022-09-29 17:39:27 +0300

    Last changes #99
    Last changes #99

---
+++ main/1_EDU/List of Controls_TRIM_1_EDU.sql
@@ -0,0 +1,52 @@
+﻿SELECT 
+      L.CONTROL,
+      L.FORMULA,
+    --  L.SQL_TEXT,
+      L.PRIORITATEA,
+      L.STATUT
+      
+      
+        
+        FROM
+
+( 
+SELECT 
+      B.CONTROL,
+      B.FORMULA,
+      B.SQL_TEXT, 
+      B.PRIORITATEA,
+      B.STATUT
+      
+      
+        
+        FROM CIS2.MD_CONTROL B  INNER JOIN (
+        SELECT 
+        A.CONTROL, 
+        MAX(A.CONTROL_VERS) CONTROL_VERS
+        
+        FROM CIS2.MD_CONTROL A
+        
+        
+         WHERE
+         1=1
+         AND A.FORM  = 40
+               
+         GROUP BY 
+          A.CONTROL
+         
+         ORDER BY 
+          A.CONTROL
+        
+        )  A  ON A.CONTROL = B.CONTROL AND A.CONTROL_VERS = B.CONTROL_VERS   
+        
+        
+         WHERE
+         1=1
+         AND B.FORM  = 40
+         AND B.STATUT <> '3'
+        -- AND B.FORM_VERS = 2009
+               
+         
+         
+         ORDER BY 
+          B.CONTROL ) L
