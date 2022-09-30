00c4ea0d9cf76eaf5fca46f8b692caead74999bd
eva57gr <eva57gradycdc@hotmail.com>
2022-09-30 09:59:40 +0300

    to be added #101
    to be added #101

--- main/Add  new SU-s/Add  new SU-s_4.sql
+++ main/Add  new SU-s/Add  new SU-s_4.sql
@@ -1,49 +1,49 @@
--- INSERT INTO CIS2.RENIM (
--- 
--- 
--- CUIIO,
---    CUIIO_VERS,
---    DENUMIRE,
---    EDIT_USER,
---    STATUT,
---    CUATM,
---    CFP,
---    CFOJ,
---    COCM,
---    CAEM,
---    BUGET,
---    TIP,
---    PROD,
---    FOR_CUB,
---    GENMUZEE,
---    TIPMUZEE,
---    TIP_LOCAL,
---    TIP_INST,
---    CAEM2,
---    N85_NTL,
---    N85_NTIIP,
---    N85_NDIIP,
---    N85_NPDS,
---    N85_NRIIP,
---    N85_NSIIP,
---    GENMUZEE2,
---    NFI,
---    NTII,
---    NPDS,
---    ORGANE,
---    TIP_INV,
---    RENIM_PERS,
---    ORGANE_COND,
---    GEN_INSTITUTIE,
---    IDNO
---
--- 
--- )
+ INSERT INTO CIS2.RENIM (
+ 
+ 
+ CUIIO,
+    CUIIO_VERS,
+    DENUMIRE,
+    EDIT_USER,
+    STATUT,
+    CUATM,
+    CFP,
+    CFOJ,
+    COCM,
+    CAEM,
+    BUGET,
+    TIP,
+    PROD,
+    FOR_CUB,
+    GENMUZEE,
+    TIPMUZEE,
+    TIP_LOCAL,
+    TIP_INST,
+    CAEM2,
+    N85_NTL,
+    N85_NTIIP,
+    N85_NDIIP,
+    N85_NPDS,
+    N85_NRIIP,
+    N85_NSIIP,
+    GENMUZEE2,
+    NFI,
+    NTII,
+    NPDS,
+    ORGANE,
+    TIP_INV,
+    RENIM_PERS,
+    ORGANE_COND,
+    GEN_INSTITUTIE,
+    IDNO
+
+ 
+ )
 
 
  SELECT 
     CUIIO,
-    CUIIO_VERS,
+    1054 CUIIO_VERS,
     DENUMIRE,
     EDIT_USER,
     STATUT,
@@ -84,39 +84,16 @@
                     WHERE 
                     
                     CUIIO IN (
-37107520,
-38098987,
-38672618,
-38673115,
-38673339,
-38778877,
-38927292,
-38956623,
-38986653,
-38990040,
-40025843,
-40043189,
-40423331,
-40502510,
-40712822,
-40715743,
-40747602,
-40802459,
-40813345,
-40874743,
-40964512,
-40986502,
-41050530,
-41076908,
-41153661,
-41153684,
-41153847,
-41153945,
-41154040,
-41154175
+           SELECT CUIIO
+        FROM USER_BANCU.ADD_NEW_SU
 
 
 
 ) 
                     
-             --    AND CUIIO_VERS <>  1054
+                 AND CUIIO_VERS <>  1054
+             
+             ORDER BY 
+             CUIIO 
+           
+             
--- main/Add  new SU-s/Add  new SU-s_7.sql
+++ main/Add  new SU-s/Add  new SU-s_7.sql
@@ -1,45 +1,61 @@
---
---INSERT INTO CIS2.FORM_CUIIO R (
---        CUIIO,
---        CUIIO_VERS,
---        FORM,
---        FORM_VERS,
---        STATUT 
---)
---
+
+INSERT INTO CIS2.FORM_CUIIO R (
+        CUIIO,
+        CUIIO_VERS,
+        FORM,
+        FORM_VERS,
+        STATUT 
+)
+
 
 
  SELECT 
  
         L.CUIIO,
         L.CUIIO_VERS,
-        26 FORM,
+        13 FORM,
         1004   FORM_VERS,
         '1' STATUT
        
-        FROM --USER_BANCU.ADD_NEW_SU L
-          RENIM L 
+        FROM USER_BANCU.ADD_NEW_SU L
         
         WHERE 
+        CUIIO IN (
         
-CUIIO IN (
-41428030,
-40966698,
-41446825,
-41509075,
-41479138,
-41509081,
-41560426,
-41267229,
-40874743,
-37107520,
-41154175,
-41191182,
-38500857
+SELECT     L.CUIIO
+--           L.CUIIO_VERS
+          
+      FROM (
 
-) 
-
-
- AND 
- 
- CUIIO_VERS IN (1053)                                
+SELECT     R.CUIIO,
+           R.CUIIO_VERS
+          
+      FROM (SELECT FC.CUIIO,
+                   FC.CUIIO_VERS,
+                   FC.FORM,
+                   FC.FORM_VERS,
+                   FC.STATUT
+              FROM CIS2.FORM_CUIIO  FC
+                   INNER JOIN (  SELECT CUIIO, MAX (CUIIO_VERS) CUIIO_VERS
+                                   FROM CIS2.FORM_CUIIO
+                                  WHERE FORM IN (:pFORM) AND CUIIO_VERS <= :pPERIOADA
+                               GROUP BY CUIIO) BB
+                       ON (    BB.CUIIO = FC.CUIIO
+                           AND BB.CUIIO_VERS = FC.CUIIO_VERS)
+             WHERE FC.FORM IN (:pFORM) AND FC.STATUT <> '3') FC
+           INNER JOIN CIS2.RENIM R
+               ON (R.CUIIO = FC.CUIIO AND R.CUIIO_VERS = FC.CUIIO_VERS) ) R 
+               
+               RIGHT JOIN (
+               
+               SELECT CUIIO
+        FROM USER_BANCU.ADD_NEW_SU
+               ) L ON L.CUIIO = R.CUIIO 
+               
+               WHERE 
+               R.CUIIO IS NULL 
+               
+               
+              
+        ) 
+                                        
