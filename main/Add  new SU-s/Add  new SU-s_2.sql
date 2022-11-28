0131e80a29e85760b0504fab34d52c594b2f5298
eva57gr <eva57gradycdc@hotmail.com>
2022-11-28 15:09:10 +0200

    M2

--- main/Add  new SU-s/Add  new SU-s_2.sql
+++ main/Add  new SU-s/Add  new SU-s_2.sql
@@ -1,15 +1,27 @@
  SELECT *
-        FROM USER_BANCU.ADD_NEW_SU;
+        FROM USER_BANCU.ADD_NEW_SU
+        
+        
+        ORDER BY 
+        CUATM DESC
+        ;
 --        
         UPDATE USER_BANCU.ADD_NEW_SU 
         
-        SET CUIIO_VERS = 2011
+        SET CUIIO_VERS = 2011,
+        CUATM = REPLACE(CUATM,'O','0')
 --        ,
 --          CUATM  = '0'||CUATM
         ;
         
         
              SELECT CUIIO
+        FROM USER_BANCU.ADD_NEW_SU;
+        
+        
+        
+      SELECT 
+      CUIIO,
+      REPLACE(CUATM,'O','0') CUATM_REPLACE,
+      CUATM              
         FROM USER_BANCU.ADD_NEW_SU
-        
-        
