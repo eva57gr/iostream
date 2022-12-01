bba43b5b3f1d193beb78d34cc9c3f171c78169fc
eva57gr <eva57gradycdc@hotmail.com>
2022-12-01 08:31:13 +0200

    updates

---
+++ main/9_AGR/9_AGR_Step_5.sql
@@ -0,0 +1,105 @@
+-- INSERT INTO CIS2.RENIM (
+-- 
+-- 
+-- CUIIO,
+--    CUIIO_VERS,
+--    DENUMIRE,
+--    EDIT_USER,
+--    STATUT,
+--    CUATM,
+--    CFP,
+--    CFOJ,
+--    COCM,
+--    CAEM,
+--    BUGET,
+--    TIP,
+--    PROD,
+--    FOR_CUB,
+--    GENMUZEE,
+--    TIPMUZEE,
+--    TIP_LOCAL,
+--    TIP_INST,
+--    CAEM2,
+--    N85_NTL,
+--    N85_NTIIP,
+--    N85_NDIIP,
+--    N85_NPDS,
+--    N85_NRIIP,
+--    N85_NSIIP,
+--    GENMUZEE2,
+--    NFI,
+--    NTII,
+--    NPDS,
+--    ORGANE,
+--    TIP_INV,
+--    RENIM_PERS,
+--    ORGANE_COND,
+--    GEN_INSTITUTIE,
+--    IDNO
+--
+-- 
+-- )
+
+
+ SELECT 
+    CUIIO,
+    CUIIO_VERS,
+    DENUMIRE,
+    EDIT_USER,
+    STATUT,
+    CUATM,
+    CFP,
+    CFOJ,
+    COCM,
+    CAEM,
+    BUGET,
+    TIP,
+    PROD,
+    FOR_CUB,
+    GENMUZEE,
+    TIPMUZEE,
+    TIP_LOCAL,
+    TIP_INST,
+    CAEM2,
+    N85_NTL,
+    N85_NTIIP,
+    N85_NDIIP,
+    N85_NPDS,
+    N85_NRIIP,
+    N85_NSIIP,
+    GENMUZEE2,
+    NFI,
+    NTII,
+    NPDS,
+    ORGANE,
+    TIP_INV,
+    RENIM_PERS,
+    ORGANE_COND,
+    GEN_INSTITUTIE,
+    IDNO
+
+                    FROM USER_BANCU.VW_MAX_RENIM_CIS2
+                       --   USER_BANCU.VW_MAX_RENIM_TRIM_CIS2
+                    
+                    WHERE 
+                    
+                    CUIIO IN (
+   SELECT 
+          D.CUIIO
+                
+            
+                FROM USER_BANCU.VW_KATALOG_29_AGRO_TRIM_4_22 D
+
+                    WHERE 
+                     1=1 
+                
+
+                  
+ ) 
+                 
+             --   AND CUIIO_VERS    =   2011
+ 
+             ORDER BY 
+             CUIIO 
+           
+             
