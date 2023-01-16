05e9091086069bcf3789e03d37f235b251c4525e
eva57gr <eva57gradycdc@hotmail.com>
2023-01-16 14:16:17 +0200

    6 #203
     6 #203

--- main/Add  new SU-s/Add  new SU-s_2.sql
+++ main/Add  new SU-s/Add  new SU-s_2.sql
@@ -7,8 +7,8 @@
         ;
 --        
         UPDATE USER_BANCU.ADD_NEW_SU 
-        
-        SET CUIIO_VERS = 2011
+        SET CUATM = CASE WHEN (LENGTH(CUATM) = 6 AND (SUBSTR(CUATM,1,1) = 1  OR SUBSTR(CUATM,1,1) = 3 )) THEN '0'||CUATM ELSE CUATM END
+       -- SET CUIIO_VERS = 2011
         --CUATM = '0'||REPLACE(CUATM,'O','0')
 --        ,
 --          CUATM  = '0'||CUATM
