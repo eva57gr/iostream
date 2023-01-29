5ac213033c3f330373b599c50ceaddd69e027176
eva57gr <eva57gradycdc@hotmail.com>
2023-01-29 09:54:19 +0200

    SQL Apeduct Canalizare

--- main/Add  new SU-s/Add  new SU-s_4.sql
+++ main/Add  new SU-s/Add  new SU-s_4.sql
@@ -45,50 +45,19 @@
 
 SELECT 
     CUIIO,
-    1056  CUIIO_VERS,
-    DENUMIRE,
-    EDIT_USER,
-    STATUT,
-    CUATM,
-    CFP,
-    CFOJ,
-    COCM,
-    CAEM,
-    BUGET,
-    TIP,
-    PROD,
-    FOR_CUB,
-    GENMUZEE,
-    TIPMUZEE,
-    TIP_LOCAL,
-    TIP_INST,
-    CAEM2,
-    N85_NTL,
-    N85_NTIIP,
-    N85_NDIIP,
-    N85_NPDS,
-    N85_NRIIP,
-    N85_NSIIP,
-    GENMUZEE2,
-    NFI,
-    NTII,
-    NPDS,
-    ORGANE,
-    TIP_INV,
-    RENIM_PERS,
-    ORGANE_COND,
-    GEN_INSTITUTIE,
     IDNO
 
                     FROM  USER_BANCU.VW_MAX_RENIM_CIS2
+                    
+                    
                           --`USER_BANCU.VW_MAX_RENIM_TRIM_CIS2
-                    
-                    WHERE 
-                    
-                    CUIIO IN (
-            SELECT CUIIO 
-        FROM USER_BANCU.ADD_NEW_1_TUR
-        
-        ) 
+--                    
+--                    WHERE 
+--                    
+--                    CUIIO IN (
+--            SELECT CUIIO 
+--        FROM USER_BANCU.ADD_NEW_1_TUR
+--        
+--        ) 
         
      --   AND CUIIO_VERS <>   2011
