71d45c8102eb06faf685a444b8351698da182bcf
eva57gr <eva57gradycdc@hotmail.com>
2023-02-27 16:14:03 +0200

    17

---
+++ main/85 EDU/Alte.sql
@@ -0,0 +1,22 @@
+                SELECT 
+                
+                    D.ANUL,
+                    ROUND(SUM(CASE WHEN D.RIND IN ('020','030') THEN NVAL(D.COL9) ELSE 0 END) / SUM(CASE WHEN D.RIND IN ('020','030') THEN NVAL(D.COL1) ELSE 0 END) * 100,1)  AS REZULT  
+                
+                        FROM CIS2.VW_DATA_ALL D
+                        
+                        WHERE 
+                        --D.PERIOADA IN (:pPERIOADA)
+                        --AND 
+                        D.FORM =  17 
+                        AND D.CAPITOL IN (1004)
+                        AND D.RIND IN ('020','030') 
+                        AND D.CFP IN ('15','16','28')
+                        GROUP BY 
+                        D.ANUL    
+                        
+                        
+                        
+                        ORDER BY 
+                        D.ANUL
+                        
