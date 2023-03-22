d943c0ce1f207d198fa72edd1399fb06ef7d027b
eva57gr <eva57gradycdc@hotmail.com>
2023-03-22 09:43:09 +0200

    Tab.15 b Anul Diferenta Pachet - cumulativ
    Tab.15 b Anul Diferenta Pachet - cumulativ

--- main/EI 78/List of Controls_TRIM_44.sql
+++ main/EI 78/List of Controls_TRIM_44.sql
@@ -29,7 +29,7 @@ SELECT
         
          WHERE
          1=1
-         AND A.FORM  = 48
+         AND A.FORM  = 64
                
          GROUP BY 
           A.CONTROL
@@ -42,7 +42,7 @@ SELECT
         
          WHERE
          1=1
-         AND B.FORM  = 48
+         AND B.FORM  = 64
          AND B.STATUT <> '3'
         -- AND B.FORM_VERS = 2009
                
@@ -53,8 +53,8 @@ SELECT
           
           
           WHERE 
-          
-           L.FORMULA LIKE '%Cap.2%'
+          1=1
+           --AND L.FORMULA LIKE '%Cap.2%'
           
           
           
