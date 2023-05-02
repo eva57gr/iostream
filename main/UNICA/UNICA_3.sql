abefd2f93ec7eb26a9a581905f0c9058143880a1
eva57gr <eva57gradycdc@hotmail.com>
2023-05-02 09:06:33 +0300

    10 #364
     10 #364

---
+++ main/UNICA/UNICA_3.sql
@@ -0,0 +1,13 @@
+SELECT *
+            FROM USER_BANCU.UNICA_1056;
+            
+            
+            SELECT CUIIO,
+                   CUIIO_VERS,
+                   CUATM,
+                   DENUMIRE
+            FROM USER_BANCU.UNICA_1056
+            
+            
+            
+            ;
---
+++ main/UNICA/UNICA_5.sql
@@ -0,0 +1,38 @@
+DECLARE -- ====================================================================
+
+  CURSOR C IS
+        
+            SELECT CUIIO,
+                   CUIIO_VERS,
+                   CUATM,
+                   DENUMIRE
+            FROM USER_BANCU.UNICA_1056
+            
+          
+          ;
+
+BEGIN -- ======================================================================
+  FOR CR IN C
+  LOOP
+    UPDATE CIS2.RENIM SET 
+      
+     --   CAEM2 = CR.CAEM2,
+        DENUMIRE = CR.DENUMIRE,
+        CUATM = CR.CUATM
+       -- CFP = CR.CFP,
+--      -- CFOJ = CR.CFOJ
+        --IDNO = CR.IDNO
+ --     
+  --  ETAPA_PROD = CR.ETAPA_PROD
+    
+    
+    WHERE 
+      CUIIO  = CR.CUIIO 
+      AND
+      CUIIO_VERS = CR.CUIIO_VERS 
+      
+      
+      
+    ;
+  END LOOP;
+END; -- =======================================================================
