66c6e992b52a621faf9a53b61401cb4193bf9bd6
eva57gr <eva57gradycdc@hotmail.com>
2023-05-31 08:07:49 +0300

    FR #378
     FR #378

--- main/Add  new SU-s/Add  new SU-s_4.sql
+++ main/Add  new SU-s/Add  new SU-s_4.sql
@@ -39,7 +39,7 @@
 --
 -- 
 -- )
---
+
 
 
 
@@ -84,43 +84,20 @@ SELECT
                     FROM  --USER_BANCU.VW_MAX_RENIM_CIS2
                     
                     
-                          USER_BANCU.VW_MAX_RENIM_TRIM_CIS2
+                          USER_BANCU.VW_MAX_RENIM_CIS2
                     
                     WHERE 
                     
                     CUIIO IN (
-            SELECT    
-            
-           R.CUIIO
-          -- R.CUIIO_VERS
-          
-      FROM (
-      SELECT FC.CUIIO,
-                   FC.CUIIO_VERS,
-                   FC.FORM,
-                   FC.FORM_VERS,
-                   FC.STATUT
-              FROM CIS2.FORM_CUIIO  FC
-                   INNER JOIN (  SELECT CUIIO, MAX (CUIIO_VERS) CUIIO_VERS
-                                   FROM CIS2.FORM_CUIIO
-                                  WHERE FORM IN (:pFORM) AND CUIIO_VERS <= :pPERIOADA
-                               GROUP BY CUIIO) BB
-                       ON (    BB.CUIIO = FC.CUIIO
-                           AND BB.CUIIO_VERS = FC.CUIIO_VERS)
-             WHERE FC.FORM IN (:pFORM) AND FC.STATUT <> '3'
-             
-             
-             ) FC
-           INNER JOIN CIS2.RENIM R
-               ON (R.CUIIO = FC.CUIIO AND R.CUIIO_VERS = FC.CUIIO_VERS)
-               
-               
-               WHERE 
-               
-               R.CUIIO_VERS <>     1056
-               
+                     SELECT CUIIO
+        FROM USER_BANCU.FR
+        
 
         
         ) 
         
-        AND CUIIO_VERS  <>   1056
+--        AND 
+--        
+--        (CUIIO_VERS  <>  2012 AND 
+--        CUIIO_VERS  <>  2011 
+--        ) 
