6824ab6d53af547ab4dc12190c24f310a6e823fe
eva57gr <eva57gradycdc@hotmail.com>
2023-06-07 14:02:02 +0300

    2 LIV II

---
+++ main/Nr.2 LIV-VII/Nr.2 LIV-VII_Update_4.sql
@@ -0,0 +1,44 @@
+ SELECT *
+        FROM USER_BANCU.LIV_2
+        
+        
+        ORDER BY 
+        CUATM DESC  -- 41042980 -   44-0022
+        ;
+       
+        UPDATE USER_BANCU.LIV_2
+        SET CUATM = CASE WHEN (LENGTH(CUATM) = 6 AND (SUBSTR(CUATM,1,1) = 1  OR SUBSTR(CUATM,1,1) = 3 )) THEN '0'||CUATM ELSE CUATM END,
+         CUIIO_VERS = 1826
+        --CUATM = '0'||REPLACE(CUATM,'O','0')
+--        ,
+--          CUATM  = '0'||CUATM
+        ;
+        
+        
+             SELECT CUIIO
+        FROM USER_BANCU.LIV_2;
+        
+        
+        
+      SELECT 
+      CUIIO,
+      '0'||REPLACE(CUATM,'O','0') CUATM_REPLACE,
+      CUATM              
+        FROM USER_BANCU.ADD_NEW_SU;
+        
+        
+        
+        
+        SELECT 
+        CUIIO,
+        COUNT (CUIIO ) AS CNT
+        
+        FROM USER_BANCU.ADD_NEW_SU
+        
+        GROUP BY 
+        CUIIO
+        
+        ORDER BY 
+        
+        COUNT (CUIIO )
+        
