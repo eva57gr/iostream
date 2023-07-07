f089e1792499fd956e420bfdd912b175e76d813b
eva57gr <eva57gradycdc@hotmail.com>
2023-07-07 12:58:37 +0300

    The update

---
+++ main/USERS/INSERT_USER.sql
@@ -0,0 +1,89 @@
+--INSERT INTO    CIS2.SYS_USER_ACCES
+--    (
+--  ID_USER,
+--  CUATM,
+--  FORM,
+--  FORM_VERS,
+--  ISADMIN,
+--  ACCES_TYPE,
+--  CAPITOL_ACCES,
+--  DATA_REG
+--  )
+
+
+
+
+
+SELECT
+  1612 ID_USER,
+  L.CUATM,
+  L.FORM,
+  L.FORM_VERS,
+  L.ISADMIN,
+  L.ACCES_TYPE,
+  L.CAPITOL_ACCES,
+  SYSDATE DATA_REG
+
+FROM
+
+(
+  SELECT
+  ID_USER,
+  CUATM,
+  FORM,
+  FORM_VERS,
+  ISADMIN,
+  ACCES_TYPE,
+  CAPITOL_ACCES,
+  DATA_REG
+
+     FROM CIS2.SYS_USER_ACCES
+
+
+        WHERE
+
+        ID_USER IN (63)
+
+
+
+
+
+        ) L  LEFT JOIN  (
+
+        SELECT
+  ID_USER,
+  CUATM,
+  FORM,
+  FORM_VERS,
+  ISADMIN,
+  ACCES_TYPE,
+  CAPITOL_ACCES,
+  DATA_REG
+
+     FROM CIS2.SYS_USER_ACCES
+
+
+        WHERE
+
+        ID_USER IN (1612)
+
+        ) R ON R.FORM = L.FORM
+
+
+        WHERE
+
+        R.FORM IS NULL
+
+
+GROUP BY
+  L.CUATM,
+  L.FORM,
+  L.FORM_VERS,
+  L.ISADMIN,
+  L.ACCES_TYPE,
+  L.CAPITOL_ACCES,
+  ROWNUM
+
+      HAVING
+
+      ROWNUM <=10
