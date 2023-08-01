022035b2c69f91a7a0cbcf1ee38ebf5da2b9b6c2
eva57gr <eva57gradycdc@hotmail.com>
2023-08-01 16:37:50 +0300

    Next level

---
+++ main/Monitoring_FISC/DUAL.sql
@@ -0,0 +1,6 @@
+
+
+
+        SELECT SYSDATE 
+        
+                FROM DUAL
---
+++ main/Monitoring_FISC/MONITORING_FISC.sql
@@ -0,0 +1,28 @@
+SELECT
+DISTINCT D.CUIIO,
+         D.FORM, 
+       MAX(D.DATA_REG) DATA_REG         
+            
+            FROM USER_EREPORTING.VW_DATA_ALL_PRIMIT D
+            
+            WHERE 
+            1=1
+            
+             AND D.DATA_REG > TO_DATE('07/28/2023 00:01:00', 'MM/DD/YYYY HH24:MI:SS')
+            
+--            D.FORM IN (:pFORM)
+--            AND D.PERIOADA IN (:pPERIOADA)
+            
+            GROUP BY
+            D.FORM, 
+            D.CUIIO
+     
+            
+           HAVING 
+           
+           MAX(D.DATA_REG) > TO_DATE('07/28/2023 00:01:00', 'MM/DD/YYYY HH24:MI:SS')
+            
+            ORDER BY
+            MAX(D.DATA_REG) DESC
+            
+            
