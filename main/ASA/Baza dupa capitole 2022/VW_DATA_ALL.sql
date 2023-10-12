37a5485702a9717ba2ec01c710284779835b6fc8
eva57gr <eva57gradycdc@hotmail.com>
2023-10-12 16:13:03 +0300

    гзвфеу

--- main/ASA/Baza dupa capitole 2022/VW_DATA_ALL.sql
+++ main/ASA/Baza dupa capitole 2022/VW_DATA_ALL.sql
@@ -1,3 +1,11 @@
+SELECT 
+L.* 
+
+
+FROM 
+
+(
+
 SELECT
 DISTINCT 
 D.CUIIO
@@ -5,8 +13,30 @@ FROM
  CIS2.VW_DATA_ALL D 
  WHERE
   (D.PERIOADA=:pPERIOADA) AND
-  (D.FORM = :pFORM ) 
+  (D.FORM = :pFORM ) ) L LEFT JOIN (
+  
+  
+   
            
   
   
   
+        SELECT FC.CUIIO,
+                   FC.CUIIO_VERS,
+                   FC.FORM,
+                   FC.FORM_VERS,
+                   FC.STATUT
+              FROM CIS2.FORM_CUIIO  FC
+                   INNER JOIN (  SELECT CUIIO, MAX (CUIIO_VERS) CUIIO_VERS
+                                   FROM CIS2.FORM_CUIIO
+                                  WHERE FORM IN (45) AND CUIIO_VERS <= 1058
+                               GROUP BY CUIIO) BB
+                       ON (    BB.CUIIO = FC.CUIIO
+                           AND BB.CUIIO_VERS = FC.CUIIO_VERS)
+             WHERE FC.FORM IN (45) AND FC.STATUT <> '3'
+  
+  
+  ) R ON R.CUIIO = L.CUIIO
+  
+  WHERE 
+  R.CUIIO IS  NULL 
