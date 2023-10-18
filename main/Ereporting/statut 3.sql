123def5736676ed54072868f4771530ec85482bf
eva57gr <eva57gradycdc@hotmail.com>
2023-10-18 08:48:13 +0300

    next

--- main/Ereporting/statut 3.sql
+++ main/Ereporting/statut 3.sql
@@ -2,10 +2,14 @@
           SELECT
             DISTINCT
             R.CUIIO,
-            FC.CUIIO_VERS INTO vCUIIO, vCUIIO_VERS
+            FC.CUIIO_VERS --INTO vCUIIO, vCUIIO_VERS
           FROM
             RENIM R
-            INNER JOIN FORM_CUIIO FC ON (R.CUIIO=FC.CUIIO AND R.CUIIO_VERS=FC.CUIIO_VERS AND R.ID_SCHEMA=FC.ID_SCHEMA)
+            INNER JOIN FORM_CUIIO FC ON (R.CUIIO=FC.CUIIO AND R.CUIIO_VERS=FC.CUIIO_VERS 
+            
+            --AND R.ID_SCHEMA=FC.ID_SCHEMA
+            
+            )
             INNER JOIN (
           SELECT
             DISTINCT
@@ -13,17 +17,22 @@
                 MAX(R.CUIIO_VERS) AS CUIIO_VERS
               FROM
                 RENIM R
-                INNER JOIN FORM_CUIIO FC ON (R.CUIIO=FC.CUIIO AND R.CUIIO_VERS=FC.CUIIO_VERS AND R.ID_SCHEMA=FC.ID_SCHEMA)
+                INNER JOIN FORM_CUIIO FC ON (R.CUIIO=FC.CUIIO AND R.CUIIO_VERS=FC.CUIIO_VERS 
+                
+                --AND R.ID_SCHEMA=FC.ID_SCHEMA
+                )
               WHERE
-                R.CUIIO_VERS <= vPERIOADA AND
-                FC.FORM IN (vFORM)
+                R.CUIIO_VERS <= :vPERIOADA AND
+                FC.FORM IN (:vFORM)
               GROUP BY
                 R.CUIIO) D ON (R.CUIIO=D.CUIIO AND R.CUIIO_VERS=D.CUIIO_VERS)
           WHERE
-            FC.FORM IN (vFORM) AND
+            FC.FORM IN (:vFORM) AND
             FC.STATUT <> '3' AND
-            FC.CUIIO IN (vCUIIO) AND
-            FC.ID_SCHEMA=vID_SCHEMA;
+            FC.CUIIO IN (:vCUIIO) 
+            
+--            AND
+--            FC.ID_SCHEMA=vID_SCHEMA;
 --        EXCEPTION
 --          WHEN NO_DATA_FOUND THEN
 --            UPDATE F_XML_FORMS SET STATUS=3, PROCESSING_MESSAGE = 'Intreprinderea dumneavoastra nu a nimerit in esantion.' WHERE FORMID IN (vFROMID);
