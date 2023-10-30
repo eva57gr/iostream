712fe2212214c5c90607b94b6ad834cdf37d8ada
eva57gr <eva57gradycdc@hotmail.com>
2023-10-30 16:10:10 +0200

    update

---
+++ main/renim/UPDATE_IDNO.sql
@@ -0,0 +1,13 @@
+--UPDATE RENIM 
+--         SET IDNO = '1023601000050'
+--               
+         
+         SELECT *
+         
+                FROM CIS2.RENIM 
+               
+               WHERE 
+               
+               CUIIO = 40040475
+                    
+                    
