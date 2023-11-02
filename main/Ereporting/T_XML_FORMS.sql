b6c6855a80b9519f2e9fbd2baa124a0e6def77d2
eva57gr <eva57gradycdc@hotmail.com>
2023-11-02 09:00:44 +0200

    update

--- main/Ereporting/T_XML_FORMS.sql
+++ main/Ereporting/T_XML_FORMS.sql
@@ -23,42 +23,50 @@ SELECT
           
           WHERE
 
- 
+STATUS  =  '5' AND 
+
+
+DATA_REG >= TO_DATE('11/01/2023 00:00:', 'MM/DD/YYYY HH24:MI:SS')
+
+-- 
 --  FORM_TYPE = 'ei-78'
--- AND STATUS <> '5' AND   
-
---AND 
-
-(STATUS = '2' 
-
---OR STATUS = '5'
-
-) 
-
-AND 
-
-(
-
-
---I'm looking for between 23 and 24. Did I write the code correctly in SQL for Oracle?
-
-DATA_REG >= TO_DATE('10/23/2023 00:00:', 'MM/DD/YYYY HH24:MI:SS')
-
-
+---- AND 
+  
+--
+----AND 
+----
+--(
+----
+--STATUS = '2' 
+----
+------OR STATUS = '5'
+----
+--) 
+--
 --AND 
 --
+----(
+----
+--
+----I'm looking for between 23 and 24. Did I write the code correctly in SQL for Oracle?
+--
+
+--
+--
+----AND 
+------
 --DATA_REG < TO_DATE('10/25/2023 00:00:', 'MM/DD/YYYY HH24:MI:SS')
-
-
-
---TO_DATE('10/23/2023 00:00:00', 'MM/DD/YYYY HH24:MI:SS') 
-
---AND   DATA_REG < TO_DATE('10/24/2023 00:00:00', 'MM/DD/YYYY HH24:MI:SS')
-)
+--
+--
+--
+----TO_DATE('10/23/2023 00:00:00', 'MM/DD/YYYY HH24:MI:SS') 
+--
+----AND   DATA_REG < TO_DATE('10/24/2023 00:00:00', 'MM/DD/YYYY HH24:MI:SS')
+--)
  
 -- AND PROCESSING_MESSAGE = 'OK'
 
 
  ORDER BY 
  
-DATA_REG ASC 
+DATA_REG DESC 
