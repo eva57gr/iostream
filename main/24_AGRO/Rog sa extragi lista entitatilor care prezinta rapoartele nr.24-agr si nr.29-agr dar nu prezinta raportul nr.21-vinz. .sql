04351d6680f973177fef1ecbd86a4ca056ab0c0d
eva57gr <eva57gradycdc@hotmail.com>
2023-11-09 14:02:17 +0200

    update

---
+++ main/24_AGRO/Rog sa extragi lista entitatilor care prezinta rapoartele nr.24-agr si nr.29-agr dar nu prezinta raportul nr.21-vinz. .sql
@@ -0,0 +1,70 @@
+SELECT 
+D.CUIIO,
+D.CUIIO_VERS,
+R.DENUMIRE,
+R.CUATM,
+R.IDNO  
+
+FROM       
+
+
+
+      (SELECT FC.CUIIO,
+                   FC.CUIIO_VERS,
+                   FC.FORM,
+                   FC.FORM_VERS,
+                   FC.STATUT
+              FROM CIS2.FORM_CUIIO  FC
+                   INNER JOIN (  SELECT CUIIO, MAX (CUIIO_VERS) CUIIO_VERS
+                                   FROM CIS2.FORM_CUIIO
+                                  WHERE FORM IN (45) AND CUIIO_VERS <= 1058
+                               GROUP BY CUIIO) BB
+                       ON (    BB.CUIIO = FC.CUIIO
+                           AND BB.CUIIO_VERS = FC.CUIIO_VERS)
+             WHERE FC.FORM IN (45) AND FC.STATUT <> '3'
+             
+             
+             
+             UNION 
+             
+                   SELECT FC.CUIIO,
+                   FC.CUIIO_VERS,
+                   FC.FORM,
+                   FC.FORM_VERS,
+                   FC.STATUT
+              FROM CIS2.FORM_CUIIO  FC
+                   INNER JOIN (  SELECT CUIIO, MAX (CUIIO_VERS) CUIIO_VERS
+                                   FROM CIS2.FORM_CUIIO
+                                  WHERE FORM IN (33) AND CUIIO_VERS <= 1058
+                               GROUP BY CUIIO) BB
+                       ON (    BB.CUIIO = FC.CUIIO
+                           AND BB.CUIIO_VERS = FC.CUIIO_VERS)
+             WHERE FC.FORM IN (33) AND FC.STATUT <> '3' ) D
+             
+             
+             
+             
+                                INNER JOIN CIS2.RENIM R ON 
+                                R.CUIIO = D.CUIIO
+                                AND R.CUIIO_VERS = D.CUIIO_VERS 
+             
+             
+             WHERE 
+             
+             
+             D.CUIIO NOT IN (
+             
+             SELECT FC.CUIIO
+--                   FC.CUIIO_VERS,
+--                   FC.FORM,
+--                   FC.FORM_VERS,
+--                   FC.STATUT
+              FROM CIS2.FORM_CUIIO  FC
+                   INNER JOIN (  SELECT CUIIO, MAX (CUIIO_VERS) CUIIO_VERS
+                                   FROM CIS2.FORM_CUIIO
+                                  WHERE FORM IN (26) AND CUIIO_VERS <= 1058
+                               GROUP BY CUIIO) BB
+                       ON (    BB.CUIIO = FC.CUIIO
+                           AND BB.CUIIO_VERS = FC.CUIIO_VERS)
+             WHERE FC.FORM IN (26) AND FC.STATUT <> '3'
+             )
---
+++ main/24_AGRO/Rog sa extragi lista entitatilor care prezinta rapoartele nr.24-agr si nr.29-agr dar nu prezinta raportul nr.21-vinz_1. .sql
@@ -0,0 +1,36 @@
+
+
+
+
+                   SELECT 
+                   DISTINCT CUIIO,
+                   USER_NAME,
+                   DATA_REG 
+
+
+
+                    FROM CIS2.VW_DATA_ALL 
+                    
+                    WHERE 
+
+
+                    FORM IN  (44)
+
+AND PERIOADA = 1058
+
+
+AND CUIIO IN (
+41157268,
+41360851,
+41428603,
+40831811,
+40252289
+
+)
+
+
+
+        ORDER BY 
+             CUIIO,
+                   USER_NAME,
+                   DATA_REG
