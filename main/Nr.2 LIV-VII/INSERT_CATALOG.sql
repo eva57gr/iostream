e26c4a9eea65bc578fe6a5a0c194f8c3538dd65a
eva57gr <eva57gradycdc@hotmail.com>
2023-12-11 11:24:02 +0200

    update

--- main/Nr.2 LIV-VII/INSERT_CATALOG.sql
+++ main/Nr.2 LIV-VII/INSERT_CATALOG.sql
@@ -1,51 +1,51 @@
- INSERT INTO CIS2.RENIM (
- 
- 
- CUIIO,
-    CUIIO_VERS,
-    DENUMIRE,
-    EDIT_USER,
-    STATUT,
-    CUATM,
-    CFP,
-    CFOJ,
-    COCM,
-    CAEM,
-    BUGET,
-    TIP,
-    PROD,
-    FOR_CUB,
-    GENMUZEE,
-    TIPMUZEE,
-    TIP_LOCAL,
-    TIP_INST,
-    CAEM2,
-    N85_NTL,
-    N85_NTIIP,
-    N85_NDIIP,
-    N85_NPDS,
-    N85_NRIIP,
-    N85_NSIIP,
-    GENMUZEE2,
-    NFI,
-    NTII,
-    NPDS,
-    ORGANE,
-    TIP_INV,
-    RENIM_PERS,
-    ORGANE_COND,
-    GEN_INSTITUTIE,
-    IDNO
-
- 
- )
+-- INSERT INTO CIS2.RENIM (
+-- 
+-- 
+-- CUIIO,
+--    CUIIO_VERS,
+--    DENUMIRE,
+--    EDIT_USER,
+--    STATUT,
+--    CUATM,
+--    CFP,
+--    CFOJ,
+--    COCM,
+--    CAEM,
+--    BUGET,
+--    TIP,
+--    PROD,
+--    FOR_CUB,
+--    GENMUZEE,
+--    TIPMUZEE,
+--    TIP_LOCAL,
+--    TIP_INST,
+--    CAEM2,
+--    N85_NTL,
+--    N85_NTIIP,
+--    N85_NDIIP,
+--    N85_NPDS,
+--    N85_NRIIP,
+--    N85_NSIIP,
+--    GENMUZEE2,
+--    NFI,
+--    NTII,
+--    NPDS,
+--    ORGANE,
+--    TIP_INV,
+--    RENIM_PERS,
+--    ORGANE_COND,
+--    GEN_INSTITUTIE,
+--    IDNO
+--
+-- 
+-- )
 
 
 
 
 SELECT 
  CUIIO,
- 1827 CUIIO_VERS,
+ CUIIO_VERS,
  DENUMIRE,
  EDIT_USER,
  STATUT,
@@ -121,9 +121,9 @@ SELECT
         
         ) 
         
-        AND 
-        
-        (
-        CUIIO_VERS  <>   1827 
-         
-        ) 
+--        AND 
+--        
+--        (
+--        CUIIO_VERS  <>   1827 
+--         
+--        ) 
