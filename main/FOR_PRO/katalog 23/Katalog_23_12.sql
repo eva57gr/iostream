8bc13698539eea872480532bc43daf7a0baed3f9
eva57gr <eva57gradycdc@hotmail.com>
2024-02-01 11:49:22 +0200

    update

--- main/FOR_PRO/katalog 23/Katalog_23_12.sql
+++ main/FOR_PRO/katalog 23/Katalog_23_12.sql
@@ -1,10 +1,10 @@
---INSERT INTO CIS2.FORM_CUIIO R (
---        CUIIO,
---        CUIIO_VERS,
---        FORM,
---        FORM_VERS,
---        STATUT 
---)
+INSERT INTO CIS2.FORM_CUIIO R (
+        CUIIO,
+        CUIIO_VERS,
+        FORM,
+        FORM_VERS,
+        STATUT 
+)
 
 
 
@@ -12,7 +12,7 @@
 SELECT 
        L.CUIIO,
         2012 CUIIO_VERS,
-        42 FORM,
+        64 FORM,
         2000    FORM_VERS,
         '1' STATUT
         
@@ -60,7 +60,7 @@ SELECT
         L.CAEM2,
         L.TIP ,
         L.BUGET
-        FROM USER_BANCU.M3 L
+        FROM USER_BANCU.ASA L
              ) R ON R.CUIIO = FC.CUIIO
              
              
@@ -75,4 +75,79 @@ SELECT
               
                
                
+              WHERE 
+              L.CUIIO NOT IN (
               
+              
+
+
+
+
+
+
+            SELECT 
+            
+               DISTINCT  D.CUIIO 
+                
+                    FROM CIS2.VW_DATA_ALL D
+                    
+                    
+                    WHERE 
+                    D.PERIOADA IN (:pPERIOADA)
+                    AND
+                    D.FORM IN (:pFORM)
+            
+                    AND D.CUIIO IN (
+                    
+                    
+                    
+                                SELECT 
+            FC.CUIIO 
+--            FC_CUIIO,
+--            R.CUIIO  R_CUIIO
+--                 
+              FROM
+              (
+      
+      
+      SELECT FC.CUIIO,
+                   FC.CUIIO_VERS,
+                   FC.FORM,
+                   FC.FORM_VERS,
+                   FC.STATUT
+              FROM CIS2.FORM_CUIIO  FC
+                   INNER JOIN (  SELECT CUIIO, MAX (CUIIO_VERS) CUIIO_VERS
+                                   FROM CIS2.FORM_CUIIO
+                                  WHERE FORM IN (:pFORM) AND CUIIO_VERS <= :pPERIOADA
+                               GROUP BY CUIIO) BB
+                       ON (    BB.CUIIO = FC.CUIIO
+                           AND BB.CUIIO_VERS = FC.CUIIO_VERS)
+             WHERE FC.FORM IN (:pFORM) AND FC.STATUT <> '3'
+             ) FC LEFT  JOIN 
+             
+             (
+             SELECT 
+ 
+        L.CUIIO,
+        L.CUIIO_VERS,
+        L.DENUMIRE,
+        L.CUATM,
+        L.CFP,
+        L.CFOJ,
+        L.CAEM2,
+        L.TIP ,
+        L.BUGET
+        FROM  USER_BANCU.ASA L
+             ) R ON R.CUIIO = FC.CUIIO
+             
+             
+             WHERE 
+             
+             R.CUIIO IS NULL  
+             
+             
+             
+   
+                    )        
+                    
+              ) 
---
+++ main/FOR_PRO/katalog 23/Katalog_23_17.sql
@@ -0,0 +1,12 @@
+SELECT 
+ 
+        L.CUIIO,
+        L.CUIIO_VERS,
+        L.DENUMIRE,
+        L.CUATM,
+        L.CFP,
+        L.CFOJ,
+        L.CAEM2,
+        L.TIP,
+        L.BUGET
+        FROM  USER_BANCU.ASA L
