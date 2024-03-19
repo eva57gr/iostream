68f6aee4c76b260a484c89cb2dd4ba99870bd255
eva57gr <eva57gradycdc@hotmail.com>
2024-03-19 16:45:21 +0200

    update

---
+++ main/16 AGRO/List of Controls_16_AGRO.sql
@@ -0,0 +1,65 @@
+﻿SELECT 
+      L.CONTROL,
+      L.FORMULA,
+     -- L.SQL_TEXT,
+      L.PRIORITATEA,
+      L.STATUT
+      
+      
+        
+        FROM
+
+( 
+SELECT 
+      B.CONTROL,
+      B.FORMULA,
+      B.SQL_TEXT,
+      B.PRIORITATEA,
+      B.STATUT
+      
+      
+        
+        FROM CIS2.MD_CONTROL B  INNER JOIN (
+        SELECT 
+        A.CONTROL, 
+        MAX(A.CONTROL_VERS) CONTROL_VERS
+        
+        FROM CIS2.MD_CONTROL A
+        
+        
+         WHERE
+         1=1
+         AND A.FORM  = 36
+               
+         GROUP BY 
+          A.CONTROL
+         
+         ORDER BY 
+          A.CONTROL
+        
+        )  A  ON A.CONTROL = B.CONTROL AND A.CONTROL_VERS = B.CONTROL_VERS   
+        
+        
+         WHERE
+         1=1
+         AND B.FORM  = 36
+         AND B.STATUT <> '3'
+        -- AND B.FORM_VERS = 2009
+               
+         
+         
+         ORDER BY 
+          B.CONTROL ) L
+          
+          
+          WHERE 
+          1=1
+--          AND  L.FORMULA LIKE '%Cap.I %'
+--
+--    AND
+--    L.CONTROL NOT IN ('54-002','54-004','54-007','54-008','54-013','54-020', '54-014','54-017','54-015','54-001','54-010', '54-005','54-006'
+--    ,'54-009','54-011','54-016') 
+
+          
+          
+          
