779d238072966a677f3d1aa4ea8d0c032402bbbd
eva57gr <eva57gradycdc@hotmail.com>
2024-04-07 06:01:29 +0300

    update

--- main/EI 78/CUMULATIV_2023/Tab. 6.1.sql
+++ main/EI 78/CUMULATIV_2023/Tab. 6.1.sql
@@ -1,47 +1,47 @@
-DECLARE
-
-  CURSOR C IS
+--DECLARE
+--
+--  CURSOR C IS
+--
+--SELECT 
+--    DF.PERIOADA,
+--    DF.FORM,
+--    DF.FORM_VERS,
+--    DF.ID_MDTABLE,
+--    DF.COD_CUATM,
+--    DF.NR_SECTIE,
+--    DF.NUME_SECTIE,
+--    DF.NR_SECTIE1,
+--    DF.NUME_SECTIE1,
+--    DF.NR_SECTIE2,
+--    DF.NUME_SECTIE2,
+--    DF.NR_ROW NR_ROW,
+--    DF.ORDINE,
+--    DF.DECIMAL_POS,
+--    DF.NUME_ROW,
+--    DF.COL1,
+--    DF.COL2,
+--    DF.COL3,
+--    DF.COL4
+--    
+--  
+--
+--   
+--FROM 
+--(
+--
 
 SELECT 
-    DF.PERIOADA,
-    DF.FORM,
-    DF.FORM_VERS,
-    DF.ID_MDTABLE,
-    DF.COD_CUATM,
-    DF.NR_SECTIE,
-    DF.NUME_SECTIE,
-    DF.NR_SECTIE1,
-    DF.NUME_SECTIE1,
-    DF.NR_SECTIE2,
-    DF.NUME_SECTIE2,
-    DF.NR_ROW NR_ROW,
-    DF.ORDINE,
-    DF.DECIMAL_POS,
-    DF.NUME_ROW,
-    DF.COL1,
-    DF.COL2,
-    DF.COL3,
-    DF.COL4
-    
-  
-
-   
-FROM 
-(
-
-
-SELECT 
-    :pPERIOADA AS PERIOADA,
-    :pFORM AS FORM,
-    :pFORM_VERS AS FORM_VERS,
-    :pID_MDTABLE AS ID_MDTABLE,
-    :pCOD_CUATM AS COD_CUATM,
-    '0' AS NR_SECTIE,
-    '0' AS NUME_SECTIE,
-    '0' AS NR_SECTIE1,
-    '0' AS NUME_SECTIE1,
-    '0' AS NR_SECTIE2,
-    '0' AS NUME_SECTIE2,
+--    :pPERIOADA AS PERIOADA,
+--    :pFORM AS FORM,
+--    :pFORM_VERS AS FORM_VERS,
+--    :pID_MDTABLE AS ID_MDTABLE,
+--    :pCOD_CUATM AS COD_CUATM,
+--    '0' AS NR_SECTIE,
+--    '0' AS NUME_SECTIE,
+--    '0' AS NR_SECTIE1,
+--    '0' AS NUME_SECTIE1,
+--    '0' AS NR_SECTIE2,
+--    '0' AS NUME_SECTIE2,
      C.ORDINE NR_ROW,
      C.ORDINE   AS ORDINE,
      '1111' AS DECIMAL_POS,
@@ -147,60 +147,60 @@ WHERE
    
   
    
-   ) DF
-  
-  ;
-   
-    BEGIN
-
-  FOR CR IN C
-  
-  LOOP
-    INSERT INTO  -- USER_BANCU.TABLE_OUT_TEST 
-    
-     CIS2.TABLE_OUT
-    (
-      PERIOADA,
-      FORM,
-      FORM_VERS,
-
-      ID_MDTABLE,
-      COD_CUATM,
-      NR_SECTIE,
-      NUME_SECTIE,
-      NR_SECTIE1,
-      NUME_SECTIE1,
-      NR_SECTIE2,
-      NUME_SECTIE2,
-      NR_ROW,
-      ORDINE,
-      DECIMAL_POS,
-      NUME_ROW,
-       
-      COL1, COL2, COL3,  COL4
-    )
-    VALUES
-    (
-      CR.PERIOADA,
-      CR.FORM,
-      CR.FORM_VERS,
-      CR.ID_MDTABLE,
-      CR.COD_CUATM,
-      CR.NR_SECTIE,
-      CR.NUME_SECTIE,
-      CR.NR_SECTIE1,
-      CR.NUME_SECTIE1,
-      CR.NR_SECTIE2,
-      CR.NUME_SECTIE2,
-      CR.NR_ROW,
-      CR.ORDINE,
-      CR.DECIMAL_POS,
-      CR.NUME_ROW,
-       
-      CR.COL1, CR.COL2, CR.COL3, CR.COL4
-    );
-  END LOOP;
-END;
-  
+--   ) DF
+--  
+--  ;
+--   
+--    BEGIN
+--
+--  FOR CR IN C
+--  
+--  LOOP
+--    INSERT INTO  -- USER_BANCU.TABLE_OUT_TEST 
+--    
+--     CIS2.TABLE_OUT
+--    (
+--      PERIOADA,
+--      FORM,
+--      FORM_VERS,
+--
+--      ID_MDTABLE,
+--      COD_CUATM,
+--      NR_SECTIE,
+--      NUME_SECTIE,
+--      NR_SECTIE1,
+--      NUME_SECTIE1,
+--      NR_SECTIE2,
+--      NUME_SECTIE2,
+--      NR_ROW,
+--      ORDINE,
+--      DECIMAL_POS,
+--      NUME_ROW,
+--       
+--      COL1, COL2, COL3,  COL4
+--    )
+--    VALUES
+--    (
+--      CR.PERIOADA,
+--      CR.FORM,
+--      CR.FORM_VERS,
+--      CR.ID_MDTABLE,
+--      CR.COD_CUATM,
+--      CR.NR_SECTIE,
+--      CR.NUME_SECTIE,
+--      CR.NR_SECTIE1,
+--      CR.NUME_SECTIE1,
+--      CR.NR_SECTIE2,
+--      CR.NUME_SECTIE2,
+--      CR.NR_ROW,
+--      CR.ORDINE,
+--      CR.DECIMAL_POS,
+--      CR.NUME_ROW,
+--       
+--      CR.COL1, CR.COL2, CR.COL3, CR.COL4
+--    );
+--  END LOOP;
+--END;
+--  
   
    
