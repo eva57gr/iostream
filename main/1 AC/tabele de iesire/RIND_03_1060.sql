0d3e0b7f5045fb2fc9f30f9c7ba0abb8bd37ce53
eva57gr <eva57gradycdc@hotmail.com>
2024-04-11 16:40:25 +0300

    update

--- main/1 AC/tabele de iesire/RIND_03_1060.sql
+++ main/1 AC/tabele de iesire/RIND_03_1060.sql
@@ -8,11 +8,39 @@ SELECT
     CIS2.VW_DATA_ALL D INNER JOIN
     CIS2.MD_RIND R ON(D.ID_MD=R.ID_MD)
   WHERE
+    D.CUIIO = 4496728 AND 
     D.FORM = 15 AND
-    D.CAPITOL = 1000 AND
+    D.CAPITOL = 1000 
+    
+    AND
     D.PERIOADA BETWEEN FLOOR(:pPERIOADA/4)*4 AND :pPERIOADA
     AND (CIS2.D.CUATM_FULL LIKE '%'||:pCOD_CUATM||';%')
+   
     
   GROUP BY
     R.RIND,
     R.DENUMIRE
+    
+    
+    
+    UNION 
+    
+    SELECT
+    '00' AS RIND,
+    'Autorizatii de construire eliberate pentru cladiri rezidentiale - total (r.03+r.04+r.05)' AS DENUMIRE,
+    1 AS ORDINE,
+    SUM(D.COL1) AS COL1,
+    SUM(D.COL2) AS COL2
+  FROM
+    CIS2.VW_DATA_ALL D
+  WHERE
+    D.FORM = 15 AND
+    D.CAPITOL = 1000 AND
+    D.RIND IN('03', '04', '05')
+    AND D.PERIOADA BETWEEN FLOOR(:pPERIOADA/4)*4 AND :pPERIOADA
+    AND (CIS2.D.CUATM_FULL LIKE '%'||:pCOD_CUATM||';%')
+    
+--    AND D.CUIIO = 4496728
+    ORDER BY
+    ORDINE
+    
