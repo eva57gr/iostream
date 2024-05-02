cdbee360346c12a8b52dc985ffed955bf7fe38d3
eva57gr <eva57gradycdc@hotmail.com>
2024-05-02 13:52:18 +0300

    Create UPDATE_CUATM.sql

---
+++ main/renim/UPDATE_CUATM.sql
@@ -0,0 +1,45 @@
+DECLARE -- ====================================================================
+
+  CURSOR C IS
+        
+        
+  
+  
+ SELECT 
+ CUIIO,
+ CUIIO_VERS,
+ CUATM
+ 
+        FROM USER_BANCU.EI_78_CUATM    
+                            
+
+          
+          ;
+
+BEGIN -- ======================================================================
+  FOR CR IN C
+  LOOP
+    UPDATE CIS2.RENIM SET 
+      
+--    --    CAEM2 = CR.CAEM2,
+--         DENUMIRE = CR.DENUMIRE,
+       CUATM = CR.CUATM
+--         CFP = CR.CFP,
+--        CFOJ = CR.CFOJ
+    --     IDNO = CR.IDNO
+ --     
+  --  ETAPA_PROD = CR.ETAPA_PROD
+    
+    
+    WHERE 
+      CUIIO  = CR.CUIIO 
+      AND
+      CUIIO_VERS = CR.CUIIO_VERS 
+      
+      
+      
+    ;
+  END LOOP;
+END; -- =======================================================================
+
+
