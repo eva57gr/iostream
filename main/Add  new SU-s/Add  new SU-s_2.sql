10d64f8a39c77d8c80013981a08200fb151d8dbd
eva57gr <eva57gradycdc@hotmail.com>
2024-05-03 09:09:24 +0300

    update

--- main/Add  new SU-s/Add  new SU-s_2.sql
+++ main/Add  new SU-s/Add  new SU-s_2.sql
@@ -1,15 +1,15 @@
- SELECT CUIIO
-        FROM USER_BANCU.C5
+ SELECT *
+        FROM USER_BANCU.EI_78_CUATM
         
-        
-        ORDER BY 
-        CUATM DESC  -- 41042980 -   44-0022
+--        
+--        ORDER BY 
+--        CUATM DESC  -- 41042980 -   44-0022
         ;
        
-        UPDATE USER_BANCU.PROD_2023
+        UPDATE USER_BANCU.EI_78_CUATM
         SET
-        -- CUATM = CASE WHEN (LENGTH(CUATM) = 6 AND (SUBSTR(CUATM,1,1) = 1  OR SUBSTR(CUATM,1,1) = 3 )) THEN '0'||CUATM ELSE CUATM END,
-         CUIIO_VERS = 2468
+--         CUATM = CASE WHEN (LENGTH(CUATM) = 6 AND (SUBSTR(CUATM,1,1) = 1  OR SUBSTR(CUATM,1,1) = 3 )) THEN '0'||CUATM ELSE CUATM END
+         CUIIO_VERS = 1060
         --CUATM = '0'||REPLACE(CUATM,'O','0')
 --        ,
 --          CUATM  = '0'||CUATM
