037b209d7035d917b205b1bca349653ead55f6f1
eva57gr <eva57gradycdc@hotmail.com>
2024-05-08 11:44:09 +0300

    update

--- main/Add  new SU-s/New 1.sql
+++ main/Add  new SU-s/New 1.sql
@@ -14,13 +14,16 @@ SELECT     R.CUIIO,
               FROM CIS2.FORM_CUIIO  FC
                    INNER JOIN (  SELECT CUIIO, MAX (CUIIO_VERS) CUIIO_VERS
                                    FROM CIS2.FORM_CUIIO
-                                  WHERE FORM IN (33) AND CUIIO_VERS <= 1059
+                                  WHERE FORM IN (19) AND CUIIO_VERS <= 1060
                                GROUP BY CUIIO) BB
                        ON (    BB.CUIIO = FC.CUIIO
                            AND BB.CUIIO_VERS = FC.CUIIO_VERS)
-             WHERE FC.FORM IN (33) AND FC.STATUT <> '3') FC
+             WHERE FC.FORM IN (19) AND FC.STATUT <> '3') FC
            INNER JOIN CIS2.RENIM R
-               ON (R.CUIIO = FC.CUIIO AND R.CUIIO_VERS = FC.CUIIO_VERS) ) R 
+               ON (R.CUIIO = FC.CUIIO AND R.CUIIO_VERS = FC.CUIIO_VERS)
+               
+
+ ) R 
                
                LEFT JOIN (
                
@@ -35,22 +38,22 @@ SELECT     R.CUIIO,
               FROM CIS2.FORM_CUIIO  FC
                    INNER JOIN (  SELECT CUIIO, MAX (CUIIO_VERS) CUIIO_VERS
                                    FROM CIS2.FORM_CUIIO
-                                  WHERE FORM IN (27) AND CUIIO_VERS <= 2012
+                                  WHERE FORM IN (20) AND CUIIO_VERS <= 2012
                                GROUP BY CUIIO) BB
                        ON (    BB.CUIIO = FC.CUIIO
                            AND BB.CUIIO_VERS = FC.CUIIO_VERS)
-             WHERE FC.FORM IN (27) AND FC.STATUT <> '3') FC
+             WHERE FC.FORM IN (20) AND FC.STATUT <> '3') FC
            INNER JOIN CIS2.RENIM R
                ON (R.CUIIO = FC.CUIIO AND R.CUIIO_VERS = FC.CUIIO_VERS) 
                
                
-               WHERE 
-               
-                R.CUIIO_VERS  =  2012 
+--               WHERE 
+--               
+--                R.CUIIO_VERS  =  2012 
                
                ) L ON L.CUIIO = R.CUIIO 
                
                WHERE 
-               L.CUIIO IS NULL 
+               L.CUIIO IS  NULL 
                
                
