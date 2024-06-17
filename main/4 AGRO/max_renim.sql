7ce0b11f842fbae30eefe649f6885f3c137ae9d4
eva57gr <eva57gradycdc@hotmail.com>
2024-06-17 10:18:24 +0300

    Update max_renim.sql

--- main/4 AGRO/max_renim.sql
+++ main/4 AGRO/max_renim.sql
@@ -1,44 +1,44 @@
- INSERT INTO CIS2.RENIM (
- 
- 
- CUIIO,
-    CUIIO_VERS,
-    DENUMIRE,
-    EDIT_USER,
-    STATUT,
-    CUATM,
-    CFP,
-    CFOJ,
-    COCM,
-    CAEM,
-    BUGET,
-    TIP,
-    PROD,
-    FOR_CUB,
-    GENMUZEE,
-    TIPMUZEE,
-    TIP_LOCAL,
-    TIP_INST,
-    CAEM2,
-    N85_NTL,
-    N85_NTIIP,
-    N85_NDIIP,
-    N85_NPDS,
-    N85_NRIIP,
-    N85_NSIIP,
-    GENMUZEE2,
-    NFI,
-    NTII,
-    NPDS,
-    ORGANE,
-    TIP_INV,
-    RENIM_PERS,
-    ORGANE_COND,
-    GEN_INSTITUTIE,
-    IDNO
-
- 
- )
+-- INSERT INTO CIS2.RENIM (
+-- 
+-- 
+-- CUIIO,
+--    CUIIO_VERS,
+--    DENUMIRE,
+--    EDIT_USER,
+--    STATUT,
+--    CUATM,
+--    CFP,
+--    CFOJ,
+--    COCM,
+--    CAEM,
+--    BUGET,
+--    TIP,
+--    PROD,
+--    FOR_CUB,
+--    GENMUZEE,
+--    TIPMUZEE,
+--    TIP_LOCAL,
+--    TIP_INST,
+--    CAEM2,
+--    N85_NTL,
+--    N85_NTIIP,
+--    N85_NDIIP,
+--    N85_NPDS,
+--    N85_NRIIP,
+--    N85_NSIIP,
+--    GENMUZEE2,
+--    NFI,
+--    NTII,
+--    NPDS,
+--    ORGANE,
+--    TIP_INV,
+--    RENIM_PERS,
+--    ORGANE_COND,
+--    GEN_INSTITUTIE,
+--    IDNO
+--
+-- 
+-- )
 
 
 
@@ -86,7 +86,7 @@ SELECT
                            USER_BANCU.VW_MAX_RENIM_CIS2
                     
                     
-                    --     USER_BANCU.VW_MAX_RENIM_TRIM_CIS2
+                  --       USER_BANCU.VW_MAX_RENIM_TRIM_CIS2
                     
                     WHERE 
                   
@@ -97,34 +97,20 @@ SELECT
 
 
 CUIIO IN (
-41233124,
-41375485,
-40967290,
-40275184,
-41070917,
-41232024,
-40379630,
-40713514,
-40711641,
-41002057,
-41188607,
-37759814,
-40964713,
-41392408,
-41189274,
-40758575,
-41316850
+41002844,
+41252788,
+37670132,
+41476708,
+41188263
 
 )
-
-
 
         
         AND 
         
         
         CUIIO_VERS  <>  2013 
-----         
+         
          
 
 ORDER BY 
