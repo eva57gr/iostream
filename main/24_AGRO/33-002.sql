40776fb4f0359c78f36fd3f9e2f490640917cef7
eva57gr <eva57gradycdc@hotmail.com>
2024-06-18 07:19:31 +0300

    update

---
+++ main/24_AGRO/33-002.sql
@@ -0,0 +1,43 @@
+SELECT DISTINCT 
+DECODE(CC.NR_COLUMN, '1', 'COL1 ','2', 'COL2 ', '3', 'COL3 ', '4', 'COL4 ', '5', 'COL5 ', '6', 'COL6 ')||': '|| 
+'Rind.2='||  
+    CIS2.NVAL(SUM(CASE WHEN D.RIND IN('2') THEN DECODE(CC.NR_COLUMN, '1', D.COL1, '3', D.COL3, '4', D.COL4, '5', D.COL5, '6', D.COL6) ELSE 0 END))|| 
+', iar Rind.3=' ||
+    CIS2.NVAL(SUM(CASE WHEN D.RIND IN('3') THEN DECODE(CC.NR_COLUMN, '1', D.COL1, '3', D.COL3, '4', D.COL4, '5', D.COL5, '6', D.COL6) ELSE 0 END))
+  
+  AS REZULTAT
+
+FROM
+  CIS2.VW_DATA_ALL_TEMP D,             
+       (                                                                        
+       SELECT '1' AS NR_COLUMN FROM DUAL UNION                                  
+       SELECT '2' AS NR_COLUMN FROM DUAL UNION                                  
+       SELECT '3' AS NR_COLUMN FROM DUAL UNION  
+       SELECT '4' AS NR_COLUMN FROM DUAL UNION 
+       SELECT '5' AS NR_COLUMN FROM DUAL UNION 
+       SELECT '6' AS NR_COLUMN FROM DUAL                            
+       ) CC  
+WHERE
+  (D.PERIOADA=:PERIOADA  ) AND
+  (D.CUIIO=:CUIIO        ) AND
+  (D.CUIIO_VERS=:CUIIO_VERS     OR :CUIIO_VERS = -1) AND
+  (D.FORM = :FORM       ) AND
+  (D.FORM_VERS=:FORM_VERS ) AND
+  (D.CAPITOL=:CAPITOL           OR :CAPITOL = -1) AND
+  (D.CAPITOL_VERS=:CAPITOL_VERS OR :CAPITOL_VERS = -1) AND
+  (D.ID_MD=:ID_MD               OR :ID_MD = -1) AND
+  
+  D.FORM IN (33)  AND
+  D.CAPITOL IN (379) AND
+  D.RIND IN ('2','3')
+GROUP BY
+  CC.NR_COLUMN
+HAVING
+  ( CIS2.NVAL(SUM(CASE WHEN D.RIND IN('2') THEN DECODE(CC.NR_COLUMN, '1', D.COL1, '3', D.COL3, '4', D.COL4, '5', D.COL5, '6', D.COL6) ELSE 0 END))<> 0 AND
+    CIS2.NVAL(SUM(CASE WHEN D.RIND IN('3') THEN DECODE(CC.NR_COLUMN, '1', D.COL1, '3', D.COL3, '4', D.COL4, '5', D.COL5, '6', D.COL6) ELSE 0 END))= 0
+  ) 
+  OR
+  
+ (  CIS2.NVAL(SUM(CASE WHEN D.RIND IN('2') THEN DECODE(CC.NR_COLUMN, '1', D.COL1, '3', D.COL3, '4', D.COL4, '5', D.COL5, '6', D.COL6) ELSE 0 END))= 0 AND
+    CIS2.NVAL(SUM(CASE WHEN D.RIND IN('3') THEN DECODE(CC.NR_COLUMN, '1', D.COL1, '3', D.COL3, '4', D.COL4, '5', D.COL5, '6', D.COL6) ELSE 0 END))<> 0
+  ) 
