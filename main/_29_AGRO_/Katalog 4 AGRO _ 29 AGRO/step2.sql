e8bce2baf9dee23bb26665b2a4b97d59824f2647
eva57gr <eva57gradycdc@hotmail.com>
2024-06-29 06:55:54 +0300

    Update step2.sql

--- main/_29_AGRO_/Katalog 4 AGRO _ 29 AGRO/step2.sql
+++ main/_29_AGRO_/Katalog 4 AGRO _ 29 AGRO/step2.sql
@@ -1,41 +1,41 @@
-﻿         INSERT INTO RENIM (
-         CUIIO,
-    CUIIO_VERS,
-    DENUMIRE,
-    EDIT_USER,
-    STATUT,
-    CUATM,
-    CFP,
-    CFOJ,
-    COCM,
-    CAEM,
-    BUGET,
-    TIP,
-    PROD,
-    FOR_CUB,
-    GENMUZEE,
-    TIPMUZEE,
-    TIP_LOCAL,
-    TIP_INST,
-    CAEM2,
-    N85_NTL,
-    N85_NTIIP,
-    N85_NDIIP,
-    N85_NPDS,
-    N85_NRIIP,
-    N85_NSIIP,
-    GENMUZEE2,
-    NFI,
-    NTII,
-    NPDS,
-    ORGANE,
-    TIP_INV,
-    RENIM_PERS,
-    ORGANE_COND,
-    GEN_INSTITUTIE,
-    IDNO
-         )
-         
+﻿--         INSERT INTO RENIM (
+--         CUIIO,
+--    CUIIO_VERS,
+--    DENUMIRE,
+--    EDIT_USER,
+--    STATUT,
+--    CUATM,
+--    CFP,
+--    CFOJ,
+--    COCM,
+--    CAEM,
+--    BUGET,
+--    TIP,
+--    PROD,
+--    FOR_CUB,
+--    GENMUZEE,
+--    TIPMUZEE,
+--    TIP_LOCAL,
+--    TIP_INST,
+--    CAEM2,
+--    N85_NTL,
+--    N85_NTIIP,
+--    N85_NDIIP,
+--    N85_NPDS,
+--    N85_NRIIP,
+--    N85_NSIIP,
+--    GENMUZEE2,
+--    NFI,
+--    NTII,
+--    NPDS,
+--    ORGANE,
+--    TIP_INV,
+--    RENIM_PERS,
+--    ORGANE_COND,
+--    GEN_INSTITUTIE,
+--    IDNO
+--         )
+--         
          
          SELECT
          
@@ -79,20 +79,16 @@
             
             WHERE 
             
-            CUIIO IN (
+           CUIIO IN 
 
+(
+SELECT CUIIO
+    
+    FROM USER_BANCU.VW_4_AGRO_1061
+)
 
-SELECT FC.CUIIO
-               
-              FROM USER_BANCU.VW_4_AGRO_1061 FC 
-                  
-             
-             
-      
-            
-            )
-            
-             AND CUIIO_VERS  <>   1061
+AND 
+CUIIO_VERS   NOT IN (1061)
 --            
             ORDER BY 
             CUIIO_VERS
