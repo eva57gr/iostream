6e44d59f4c47d1dc73fb98fb4e7ab89e31d8333d
eva57gr <eva57gradycdc@hotmail.com>
2024-06-29 09:54:31 +0300

    Update step11.sql

--- main/_29_AGRO_/Katalog 4 AGRO _ 29 AGRO/step11.sql
+++ main/_29_AGRO_/Katalog 4 AGRO _ 29 AGRO/step11.sql
@@ -1,3 +1,39 @@
+
+
+SELECT FC.CUIIO,
+                   FC.CUIIO_VERS,
+                   FC.FORM,
+                   FC.FORM_VERS,
+                   FC.STATUT
+              FROM
+
+(
+SELECT FC.CUIIO,
+                   FC.CUIIO_VERS,
+                   FC.FORM,
+                   FC.FORM_VERS,
+                   FC.STATUT
+              FROM CIS2.FORM_CUIIO  FC
+                   INNER JOIN (  SELECT CUIIO, MAX (CUIIO_VERS) CUIIO_VERS
+                                   FROM CIS2.FORM_CUIIO
+                                  WHERE FORM IN (45) AND CUIIO_VERS <= 1061
+                               GROUP BY CUIIO) BB
+                       ON (    BB.CUIIO = FC.CUIIO
+                           AND BB.CUIIO_VERS = FC.CUIIO_VERS)
+             WHERE 
+             FC.FORM IN (45) AND FC.STATUT <> '3' ) FC
+             
+             
+             WHERE
+             
+             FC.CUIIO IN (
+             
+              SELECT 
+                   FC.CUIIO
+                   
+FROM
+
+(
 SELECT FC.CUIIO,
                    FC.CUIIO_VERS,
                    FC.FORM,
@@ -12,3 +48,22 @@ SELECT FC.CUIIO,
                            AND BB.CUIIO_VERS = FC.CUIIO_VERS)
              WHERE 
              FC.FORM IN (45) AND FC.STATUT <> '3'
+             
+             ) FC LEFT  JOIN (
+
+             SELECT CUIIO
+FROM USER_BANCU.VW_4_AGRO_1061
+
+
+             ) R ON R.CUIIO = FC.CUIIO 
+             
+             WHERE 
+             R.CUIIO IS   NULL 
+             
+
+             
+             
+             )
+             
+--AND FC.CUIIO_VERS <> 1061
+
