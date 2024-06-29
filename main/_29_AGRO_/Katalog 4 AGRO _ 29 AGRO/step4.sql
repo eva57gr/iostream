33f95edde1993c203b6ddca56efe757e83d1fc17
eva57gr <eva57gradycdc@hotmail.com>
2024-06-29 06:45:20 +0300

    Task #7269 #721

--- main/_29_AGRO_/Katalog 4 AGRO _ 29 AGRO/step4.sql
+++ main/_29_AGRO_/Katalog 4 AGRO _ 29 AGRO/step4.sql
@@ -1,4 +1,4 @@
-
+--
 --INSERT INTO CIS2.FORM_CUIIO (
 --                    CUIIO,
 --                    CUIIO_VERS,
@@ -9,7 +9,7 @@
 
                SELECT 
                    R.CUIIO,
-                   1057 CUIIO_VERS,
+                   1061 CUIIO_VERS,
                    45 FORM,
                    1004 FORM_VERS,
                    '1' STATUT
@@ -24,7 +24,7 @@ SELECT FC.CUIIO,
               FROM CIS2.FORM_CUIIO  FC
                    INNER JOIN (  SELECT CUIIO, MAX (CUIIO_VERS) CUIIO_VERS
                                    FROM CIS2.FORM_CUIIO
-                                  WHERE FORM IN (45) AND CUIIO_VERS <= 1057
+                                  WHERE FORM IN (45) AND CUIIO_VERS <= 1061
                                GROUP BY CUIIO) BB
                        ON (    BB.CUIIO = FC.CUIIO
                            AND BB.CUIIO_VERS = FC.CUIIO_VERS)
@@ -34,11 +34,65 @@ SELECT FC.CUIIO,
              ) FC RIGHT  JOIN (
 
              SELECT CUIIO
-FROM USER_BANCU.VW_4_AGRO_1057
+FROM USER_BANCU.VW_4_AGRO_1061
 
 
              ) R ON R.CUIIO = FC.CUIIO 
              
              WHERE 
              FC.CUIIO IS   NULL 
+             
+--             AND R.CUIIO IN (
+--             
+--
+--SELECT CUIIO 
+--
+--FROM RENIM 
+--
+--WHERE
+--
+--CUIIO IN 
+--
+--(
+--SELECT 
+--                   R.CUIIO
+--
+--FROM
+--
+--(
+--SELECT FC.CUIIO,
+--                   FC.CUIIO_VERS,
+--                   FC.FORM,
+--                   FC.FORM_VERS,
+--                   FC.STATUT
+--              FROM CIS2.FORM_CUIIO  FC
+--                   INNER JOIN (  SELECT CUIIO, MAX (CUIIO_VERS) CUIIO_VERS
+--                                   FROM CIS2.FORM_CUIIO
+--                                  WHERE FORM IN (45) AND CUIIO_VERS <= 1061
+--                               GROUP BY CUIIO) BB
+--                       ON (    BB.CUIIO = FC.CUIIO
+--                           AND BB.CUIIO_VERS = FC.CUIIO_VERS)
+--             WHERE 
+--             FC.FORM IN (45) AND FC.STATUT <> '3'
+--             
+--             ) FC RIGHT  JOIN (
+--
+--             SELECT CUIIO
+--FROM USER_BANCU.VW_4_AGRO_1061
+--
+--
+--             ) R ON R.CUIIO = FC.CUIIO 
+--             
+--             WHERE 
+--             FC.CUIIO IS   NULL 
+--)
+--
+--AND 
+--CUIIO_VERS  IN (1061)
+--
+--             
+--             ) 
               
+             
+            
+             
