efc21fd5007f336bdf4a5c02cd88543aaea4e289
eva57gr <eva57gradycdc@hotmail.com>
2024-07-03 10:22:34 +0300

    Create 33-026.sql

---
+++ main/24_AGRO/33-026.sql
@@ -0,0 +1,44 @@
+SELECT 
+'Col.'||CC.NR_COLUMN ||': '|| 
+CASE WHEN  CIS2.NVAL(SUM(CASE WHEN D.RIND IN('1')  AND D.CAPITOL IN (379) THEN DECODE(CC.NR_COLUMN, '1', D.COL1, '3', D.COL3, '4', D.COL4, '5', D.COL5, '6', D.COL6, '8', D.COL8) ELSE 0 END))= 0 
+THEN
+'Completati Tab.1.1'
+ ELSE 
+'Completati Tab.1.1'
+END    
+ AS REZULTAT
+FROM
+  CIS2.VW_DATA_ALL_TEMP D,             
+       (                                                                        
+       SELECT '1' AS NR_COLUMN FROM DUAL UNION                                  
+       SELECT '3' AS NR_COLUMN FROM DUAL UNION
+       SELECT '4' AS NR_COLUMN FROM DUAL UNION 
+       SELECT '5' AS NR_COLUMN FROM DUAL UNION 
+       SELECT '6' AS NR_COLUMN FROM DUAL UNION 
+       SELECT '8' AS NR_COLUMN FROM DUAL UNION  
+       SELECT '9' AS NR_COLUMN FROM DUAL UNION 
+       SELECT '11' AS NR_COLUMN FROM DUAL UNION 
+       SELECT '12' AS NR_COLUMN FROM DUAL                                
+       ) CC  
+WHERE
+  (D.PERIOADA=:PERIOADA         ) AND
+  (D.CUIIO=:CUIIO               ) AND
+  (D.CUIIO_VERS=:CUIIO_VERS     OR :CUIIO_VERS = -1) AND
+  (D.FORM = :FORM               ) AND
+  (D.FORM_VERS=:FORM_VERS       ) AND
+  (D.CAPITOL=:CAPITOL           OR :CAPITOL = -1) AND
+  (D.CAPITOL_VERS=:CAPITOL_VERS OR :CAPITOL_VERS = -1) AND
+  (D.ID_MD=:ID_MD               OR :ID_MD = -1) AND
+  
+  D.FORM IN (33)  AND
+  D.CAPITOL||'.'||D.RIND IN ('379.1','379.2') 
+   
+ GROUP BY CC.NR_COLUMN 
+HAVING
+
+ (
+  CIS2.NVAL(SUM(CASE WHEN D.RIND IN('1') AND D.CAPITOL IN (379) THEN DECODE(CC.NR_COLUMN, '1', D.COL1, '3', D.COL3, '4', D.COL4, '5', D.COL5, '6', D.COL6, '8', D.COL8) ELSE 0 END))>0 and
+ 
+  CIS2.NVAL(SUM(CASE WHEN D.RIND IN('2') AND D.CAPITOL IN (379) THEN DECODE(CC.NR_COLUMN, '1', D.COL1, '3', D.COL3, '4', D.COL4, '5', D.COL5, '6', D.COL6, '8', D.COL8) ELSE 0 END))=0 
+ )
+
