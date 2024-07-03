dbd361a91cbaa5aaa31acca20d68215fca2e97fd
eva57gr <eva57gradycdc@hotmail.com>
2024-07-03 16:29:06 +0300

    Update Katalog_2023_11.sql

--- main/_1_CMPA/Katalog 2023/Katalog_2023_11.sql
+++ main/_1_CMPA/Katalog 2023/Katalog_2023_11.sql
@@ -43,75 +43,33 @@ SELECT
       UNIT_CODE  IN (
       
       
-      SELECT 
-FC.UNIT_CODE
-FROM 
-(
-SELECT 
-FC.UNIT_CODE, 
-FC.UNIT_CODE_VERS,
-FC.NR_GOSP,
-FC.NR_MAPS,
-FC.FORM,
-FC.FORM_VERS,
-FC.STATUT    
-  FROM CIS2.FORM_REG_UNIT_GC  FC
-       INNER JOIN (  
-       
-       SELECT UNIT_CODE, 
-       
-       
-       
-       MAX (UNIT_CODE_VERS) UNIT_CODE_VERS,
-       NR_GOSP,
-       NR_MAPS
-                       FROM CIS2.FORM_REG_UNIT_GC
-                      WHERE FORM IN (:pFORM) AND UNIT_CODE_VERS <= :pPERIOADA
-                      
-                      
-                   GROUP BY 
-                   UNIT_CODE,
-                    NR_GOSP,
-                     NR_MAPS
-                   
-                   
-                   
-                   
-                   
-                   ) BB
-           ON (    BB.UNIT_CODE = FC.UNIT_CODE
-               AND BB.UNIT_CODE_VERS = FC.UNIT_CODE_VERS
-               AND BB.NR_GOSP = FC.NR_GOSP
-               AND BB.NR_MAPS = FC.NR_MAPS
-               
-               )
- WHERE FC.FORM IN (:pFORM) 
- 
-AND FC.STATUT <> '3'
+      249718,
+249606,
+250851,
+249807,
+249272,
+251044,
+249671,
+249949,
+250276,
+251940,
+249761,
+249556,
+250460,
+251651,
+250570,
+251179,
+252048,
+251315,
+250921,
+251630,
+250798
 
---  AND FC.UNIT_CODE_VERS <>  1060
- 
- ) FC   
- 
- 
  )
  
  
  AND UNIT_CODE_VERS <>   1060
  
- and 
- 
- UNIT_CODE NOT in (
- 
-419336,
-493336,
-469201,
-288112,
-60407,
-792311 
-
-
-)
  
  ORDER BY
  UNIT_CODE_VERS
