905c42d4d4da218367081e333c08f6123dbe33e0
eva57gr <eva57gradycdc@hotmail.com>
2024-07-04 14:07:43 +0300

    Update renim.sql

--- main/renim/renim.sql
+++ main/renim/renim.sql
@@ -6,4 +6,18 @@
                     --DENUMIRE LIKE '%Biroul%'
                     CUIIO LIKE '41005328%'
                     -----------------------------------
+                    ;
+                    
+            SELECT 
+                DISTINCT D.CUIIO
+            
+                    FROM CIS2.VW_DATA_ALL D
+                    
+                    WHERE 
+                    D.FORM IN (33)
+                    AND D.PERIOADA IN (1061) AND 
+                    --DENUMIRE LIKE '%Biroul%'
+                    D.CUIIO LIKE '41005328%'
+                    -----------------------------------
+                    
                     
