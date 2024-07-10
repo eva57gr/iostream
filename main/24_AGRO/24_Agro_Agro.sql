00726905fe7027b97062539541dc3c3c05ba93db
eva57gr <eva57gradycdc@hotmail.com>
2024-07-10 13:37:37 +0300

    Update 24_Agro_Agro.sql

--- main/24_AGRO/24_Agro_Agro.sql
+++ main/24_AGRO/24_Agro_Agro.sql
@@ -2,5 +2,27 @@
 
 
         -----------------------------------------------------
-             SELECT CUIIO
-               FROM USER_BANCU.AGRO_24_CTIF
+             SELECT L.CUIIO
+               
+               FROM USER_BANCU.AGRO_24_CTIF L  
+               
+                          LEFT JOIN (
+                          
+                          
+                                SELECT 
+                                    D.CUIIO,
+                                    D.CUATM
+                                      
+                                      FROM CIS2.VW_DATA_ALL D
+                                      
+                                            WHERE
+                                                D.FORM = 26
+                                                AND D.PERIOADA = 1061
+                          
+                          ) R ON R.CUIIO = L.CUIIO
+                          
+                          WHERE
+                          
+                          R.CUIIO IS NULL
+               
+                
