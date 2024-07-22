03384259abbe918de0ddd6e37492854fe768c336
eva57gr <eva57gradycdc@hotmail.com>
2024-07-22 08:46:45 +0300

    Update update_cfoj.sql

--- main/4 AGRO/update_cfoj.sql
+++ main/4 AGRO/update_cfoj.sql
@@ -1,15 +1,19 @@
  SELECT *
-        FROM USER_BANCU.AGRO_4;
+        FROM USER_BANCU.PRODMOLD
+        WHERE
+        TIP IS  NULL
+        
+        ;
         
 --        
 --        ORDER BY 
 --        CUATM DESC  -- 41042980 -   44-0022
         ;
        
-        UPDATE USER_BANCU.AGRO_4
+        UPDATE USER_BANCU.PRODMOLD
         SET
 --         CUATM = CASE WHEN (LENGTH(CUATM) = 6 AND (SUBSTR(CUATM,1,1) = 1  OR SUBSTR(CUATM,1,1) = 3 )) THEN '0'||CUATM ELSE CUATM END
-         CUIIO_VERS = 1061
+         CUIIO_VERS = 2012
         --CUATM = '0'||REPLACE(CUATM,'O','0')
 --        ,
 --          CUATM  = '0'||CUATM
