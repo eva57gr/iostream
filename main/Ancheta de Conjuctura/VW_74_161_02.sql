732dafdac5c456025588e286e44ef0c6104ad525
eva57gr <eva57gradycdc@hotmail.com>
2024-07-22 11:30:40 +0300

    update

---
+++ main/Ancheta de Conjuctura/VW_74_161_02.sql
@@ -0,0 +1,31 @@
+
+
+    
+    SELECT 
+    L.CUIIO    
+    
+        FROM USER_BANCU.VW_74_161 L
+                LEFT JOIN (
+                SELECT 
+  PERS_IT,
+  CIF_IT,
+  DATALIK,  
+  CUIIO,
+  ANUL           
+        
+        FROM CIS2.X_BAZA_SONDAJ
+        
+        WHERE 
+        ANUL = 2024
+        
+        AND 
+        
+        (CIF_IT IS NOT NULL  AND CIF_IT <> 0 ) 
+                ) R ON R.CUIIO = L.CUIIO
+                
+                
+                WHERE 
+                R.CUIIO IS NULL;
+                
+                
+                 
