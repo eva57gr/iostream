698a18c6e1b8b10455a044032ed360a46d35fdc5
eva57gr <eva57gradycdc@hotmail.com>
2024-07-23 09:56:39 +0300

    Update SQL2.sql

--- main/4 AGRO/SQL2.sql
+++ main/4 AGRO/SQL2.sql
@@ -10,7 +10,7 @@
                 FROM CIS2.VW_DATA_ALL D
                 
                 WHERE 
-                D.FORM IN (43)
+                D.FORM IN (3)
                 AND D.PERIOADA = :pPERIOADA_ANUL 
                 ) L LEFT JOIN (
                 
@@ -24,11 +24,11 @@
               
                    INNER JOIN (  SELECT CUIIO, MAX (CUIIO_VERS) CUIIO_VERS
                                    FROM CIS2.FORM_CUIIO
-                                  WHERE FORM IN (43) AND CUIIO_VERS <= :pPERIOADA_ANUL 
+                                  WHERE FORM IN (3) AND CUIIO_VERS <= :pPERIOADA_ANUL 
                                GROUP BY CUIIO) BB
                        ON (    BB.CUIIO = FC.CUIIO
                            AND BB.CUIIO_VERS = FC.CUIIO_VERS)
-             WHERE FC.FORM IN (43) AND FC.STATUT <> '3'  
+             WHERE FC.FORM IN (3) AND FC.STATUT <> '3'  
                 
                 ) R ON L.CUIIO = R.CUIIO
                 
