2752697ec02dc4b04a90e0cfa0ebd4ea87eb5818
eva57gr <eva57gradycdc@hotmail.com>
2024-09-30 11:31:42 +0300

    Create umarul GT care au de la 10 pana la 70 ha.sql

---
+++ main/_29_AGRO_/umarul GT care au de la 10 pana la 70 ha.sql
@@ -0,0 +1,36 @@
+SELECT 
+                D.CUIIO,
+                R.DENUMIRE,
+                D.CUATM,
+                D.RIND,
+                SUM(D.COL1) AS COL1,
+                SUM(D.COL2) AS COL2,
+                SUM(D.COL3) AS COL3,
+                SUM(D.COL4) AS COL4
+
+                FROM CIS2.VW_DATA_ALL D 
+                
+                            INNER JOIN CIS2.RENIM R ON R.CUIIO = D.CUIIO  AND R.CUIIO_VERS = D.CUIIO_VERS
+                
+                WHERE 
+                
+                D.PERIOADA IN (:pPERIOADA)
+            
+                AND D.CUATM LIKE '85%'
+                AND D.FORM IN (:pFORM)
+                
+                AND D.CAPITOL = 399
+                
+                AND D.RIND IN ('1117','1218')
+                
+                
+                GROUP BY 
+                
+                D.CUIIO,
+                R.DENUMIRE,
+                D.RIND,
+                D.CUATM
+                
+                ORDER BY
+                D.CUIIO, 
+                D.RIND
