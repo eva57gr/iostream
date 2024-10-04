c3718fa71f4c509480154cc1cedce812f86008bd
eva57gr <eva57gradycdc@hotmail.com>
2024-10-04 10:39:11 +0300

    Update RENIM_.sql

---
+++ main/_29_AGRO_/RENIM/RENIM_.sql
@@ -0,0 +1,18 @@
+
+
+
+        SELECT *
+        
+        
+            FROM CIS2.RENIM
+            
+            WHERE 
+            CUIIO LIKE 40879953||'%'
+            --AND CUIIO_VERS < 2000 
+            --AND 
+            --DENUMIRE LIKE '%CANTRINUT%' 
+            
+           ORDER BY
+           CUIIO,
+           CUIIO_VERS
+            
