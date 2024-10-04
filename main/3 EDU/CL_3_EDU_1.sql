cb15b788316b42b8a42098a257b26bb25b3235d0
eva57gr <eva57gradycdc@hotmail.com>
2024-10-04 14:37:24 +0300

    123456789

--- main/3 EDU/CL_3_EDU_1.sql
+++ main/3 EDU/CL_3_EDU_1.sql
@@ -1,1 +1,11 @@
-USER_BANCU.CL_3_EDU
+SELECT 
+         RIND,       
+         DENUMIRE,
+         GROUP_SPEC  
+        
+        FROM USER_BANCU.CL_3_EDU
+        
+        
+        WHERE 
+        LENGTH(GROUP_SPEC) = 3
+        
