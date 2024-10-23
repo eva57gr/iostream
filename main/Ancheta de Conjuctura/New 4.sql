f852f820ca9ee3716d5a44bb1992602dc1a856a1
eva57gr <eva57gradycdc@hotmail.com>
2024-10-23 14:51:56 +0300

    74

--- main/Ancheta de Conjuctura/New 4.sql
+++ main/Ancheta de Conjuctura/New 4.sql
@@ -1,45 +1,43 @@
-SELECT 
-DISTINCT CUIIO 
-FROM
-
-(
-
-
 SELECT
-  
-  DISTINCT D.CUIIO
-  FROM 
-  CIS2.VW_DATA_ALL D
+        L.CUIIO,
+        L.CUIIO CUIIO_AC,
+        R.CUIIO CUIIO_BZ,
+        R.PERS_IT
+                   FROM
+      (
+      SELECT FC.CUIIO,
+                   FC.CUIIO_VERS,
+                   FC.FORM,
+                   FC.FORM_VERS,
+                   FC.STATUT
+              FROM CIS2.FORM_CUIIO  FC
+                   INNER JOIN (  SELECT CUIIO, MAX (CUIIO_VERS) CUIIO_VERS
+                                   FROM CIS2.FORM_CUIIO
+                                  WHERE FORM IN (:pFORM) AND CUIIO_VERS <= :pPERIOADA
+
+                               GROUP BY CUIIO) BB
+                       ON (    BB.CUIIO = FC.CUIIO
+                           AND BB.CUIIO_VERS = FC.CUIIO_VERS)
+             WHERE FC.FORM IN (:pFORM) AND FC.STATUT <> '3'
+
+             ) L  INNER JOIN (
+             SELECT
+  CUIIO,
+  PERS_IT,
+
+  ANUL
+
+FROM CIS2.X_BAZA_SONDAJ
+
 WHERE
-  D.PERIOADA IN (:pPERIOADA) AND
-  D.FORM IN (:pFORM) AND 
-  D.CAPITOL IN (1197) AND
-  D.RIND IN ('1.1')
+
+ANUL = 2024
+--AND PERS_IT <> 0
+
+             ) R ON R.CUIIO = L.CUIIO
 
 
-UNION 
-
-SELECT
-  
-  DISTINCT D.CUIIO
-  FROM 
-  CIS2.VW_DATA_ALL D
-WHERE
-  D.PERIOADA IN (:pPERIOADA) AND
-  D.FORM IN (:pFORM) AND 
-  D.CAPITOL IN (1197) AND
-  D.RIND IN ('1.2')
-
-UNION   
-  SELECT
-  
-  DISTINCT D.CUIIO
-  FROM 
-  CIS2.VW_DATA_ALL D
-WHERE
-  D.PERIOADA IN (:pPERIOADA) AND
-  D.FORM IN (:pFORM) AND 
-  D.CAPITOL IN (1197) AND
-  D.RIND IN ('1.1')
-
-)
+--             WHERE
+--             
+--             L.CUIIO IS NULL
+--
