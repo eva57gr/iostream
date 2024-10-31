40b933f945f87595d63339f6487785934bcf5556
eva57gr <eva57gradycdc@hotmail.com>
2024-10-31 09:49:12 +0200

    345

---
+++ main/EDU_2/CIS2_CL_2_EDU.sql
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+        SELECT *
+        FROM USER_BANCU.CL_SPEC_2EDU_24
+        
+        ORDER BY 
+        ORDINE
+
---
+++ main/EDU_2/CL_2_EDU.sql
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+        SELECT *
+        FROM USER_BANCU.VW_CL_SPEC_2EDU_24
+        
+        ORDER BY 
+        ORDINE
+
