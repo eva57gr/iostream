6d5f123d28ea03e0bf8cbefdf4fb3fc3366dcdfb
eva57gr <eva57gradycdc@hotmail.com>
2024-11-01 09:27:37 +0200

    49

---
+++ main/EDU_2/CL_24/tab.13.1_ver_old_cl.sql
@@ -0,0 +1,193 @@
+--INSERT INTO TABLE_OUT
+--(
+--
+--
+--      PERIOADA,
+--      FORM,
+--      FORM_VERS,
+--
+--      ID_MDTABLE,
+--      COD_CUATM,
+--      NR_SECTIE,
+--      NUME_SECTIE,
+--      NR_SECTIE1,
+--      NUME_SECTIE1,
+--      NR_SECTIE2,
+--      NUME_SECTIE2,
+--      NR_ROW,
+--      ORDINE,
+--      DECIMAL_POS,
+--      NUME_ROW,   
+--         COL1,
+--         COL2,
+--         COL3,
+--         COL4,
+--         COL5,
+--         COL6,
+--         COL7,
+--         COL8,
+--         COL9,
+--         COL10,
+--         COL11,
+--         COL12,
+--         COL13,
+--         COL14,
+--         COL15
+--        
+--
+--
+--)
+
+SELECT
+    :pPERIOADA AS PERIOADA,
+    :pFORM AS FORM,
+    :pFORM_VERS AS FORM_VERS,
+    :pID_MDTABLE AS ID_MDTABLE,
+    :pCOD_CUATM AS COD_CUATM,
+    TO_NUMBER(NR_ROW)  AS  NR_SECTIE,
+     NR_ROW||'-'||NUME_ROW  AS NUME_SECTIE, 
+    '0' AS NR_SECTIE1,
+    '0' AS NUME_SECTIE1,
+    '0' AS NR_SECTIE2,
+    '0' AS NUME_SECTIE2, 
+    (CASE WHEN (rtrim(TO_NUMBER(B.codul),'0')) IS  NULL   THEN '0' 
+      WHEN (rtrim(TO_NUMBER(B.codul),'0')) = 1   THEN '10'
+       WHEN (rtrim(TO_NUMBER(B.codul),'0')) =  '73203'  THEN '732030'
+       WHEN (rtrim(TO_NUMBER(B.codul),'0')) =  '73201'  THEN '732010'
+       WHEN (rtrim(TO_NUMBER(B.codul),'0')) =  '72301'  THEN '723010'
+    
+    
+    
+    ELSE (rtrim(TO_NUMBER(B.codul),'0')) END)||'~'||ROWNUM/1.145 AS NR_ROW,
+    ROWNUM  AS ORDINE,
+   '000000000000000' AS DECIMAL_POS,
+    TRIM(B.DENUMIRE)    DENUMIRE,
+    B.COL1,
+    B.COL2,
+    B.COL3,
+    B.COL4,
+    B.COL5,
+    B.COL6,
+    B.COL7,
+    B.COL8,
+    B.COL9,
+    B.COL10,
+    B.COL11,
+    B.COL12,
+    B.COL13,
+    B.COL14,
+    B.COL15 
+
+FROM 
+
+
+(
+
+SELECT 
+
+   CCF.CODUL NR_ROW,
+   CCF.DENUMIRE NUME_ROW,
+   CC.CODUL,
+   CC.DENUMIRE,
+   CC.FULL_CODE,
+   SUM (CIS2.NVAL(D.COL1)) COL1,
+   SUM (CIS2.NVAL(D.COL2)) COL2,
+   SUM (CASE WHEN 1=1 THEN CIS2.NVAL(D.COL1)-CIS2.NVAL(D.COL2) END)  COL3,
+   SUM (CASE WHEN 1=1 THEN CIS2.NVAL(D.COL1)-CIS2.NVAL(D.COL3) END)  COL4,
+   SUM (CIS2.NVAL(D.COL3)) COL5,
+   SUM (CIS2.NVAL(D.COL7)) COL6,
+   SUM (CIS2.NVAL(D.COL8)) COL7,
+   SUM (CASE WHEN 1=1 THEN CIS2.NVAL(D.COL7)-CIS2.NVAL(D.COL8) END) COL8,
+   SUM (CASE WHEN 1=1 THEN CIS2.NVAL(D.COL7)-CIS2.NVAL(D.COL9) END) COL9,
+   SUM (CIS2.NVAL(D.COL9)) COL10,
+   SUM (CIS2.NVAL(D.COL10)) COL11,
+   SUM (CIS2.NVAL(D.COL11)) COL12,
+   SUM (CASE WHEN 1=1 THEN CIS2.NVAL(D.COL10)-CIS2.NVAL(D.COL11) END) COL13,
+   SUM (CASE WHEN 1=1 THEN CIS2.NVAL(D.COL10)-CIS2.NVAL(D.COL12) END)  COL14,
+   SUM (CIS2.NVAL(D.COL12)) COL15
+
+FROM 
+  CIS2.VW_DATA_ALL D
+
+INNER JOIN (
+
+ 
+SELECT 
+    D.CUIIO,
+    D.CUIIO_VERS,
+    D.RIND  RIND,
+    TO_CHAR(SUBSTR(D.RIND,2)) RIND_MOD,
+    D.ID_MD,
+    D.RIND_VERS
+  
+FROM 
+  CIS2.VW_DATA_ALL D  
+  
+WHERE
+  D.PERIOADA IN (:pPERIOADA) AND 
+  D.FORM_VERS = :pFORM_VERS     AND    
+  (:pID_MDTABLE=:pID_MDTABLE) AND
+  D.CUATM_FULL LIKE '%'||:pCOD_CUATM||';%' AND
+  D.FORM IN (49)                 AND 
+  D.CAPITOL IN (1049) AND     
+    
+  (
+  
+  D.RIND NOT IN 
+       
+       (
+       
+        SELECT 
+        CODUL
+        
+        FROM CIS2.CL_TARI_CS
+        
+       )
+        
+     AND 
+     
+     D.RIND NOT IN ('010','020','030','035','040','050','060','070')
+     
+       )   
+  
+ 
+  
+   ) DD ON   (DD.ID_MD = D.ID_MD AND D.CUIIO = DD.CUIIO AND D.RIND = DD.RIND AND D.RIND_VERS = DD.RIND_VERS  AND D.CUIIO_VERS = DD.CUIIO_VERS)   
+ 
+ 
+ 
+     INNER JOIN  CIS2.VW_CL_SPEC_2EDU C  ON  (ltrim(TO_NUMBER(C.codul),'0') =  DD.RIND_MOD) 
+     
+    INNER JOIN  CIS2.VW_CL_SPEC_2EDU   CC ON (C.FULL_CODE LIKE '%'||CC.CODUL||';%')
+    
+    -- add CFP 
+    
+    INNER JOIN CIS2.VW_CL_CFP CF ON (CF.CODUL = D.CFP)
+    INNER JOIN CIS2.VW_CL_CFP CCF ON (CF.FULL_CODE LIKE '%'||CCF.CODUL||';%') 
+  
+ 
+ WHERE
+  D.PERIOADA IN (:pPERIOADA) AND 
+  D.FORM_VERS = :pFORM_VERS     AND    
+  (:pID_MDTABLE=:pID_MDTABLE) AND
+  D.CUATM_FULL LIKE '%'||:pCOD_CUATM||';%' AND
+  D.FORM IN (49)                 AND 
+  D.CAPITOL IN (1049) 
+  
+ 
+ 
+ AND  CCF.CODUL IN ('00','11','14')
+ 
+  
+  GROUP BY 
+
+  CCF.CODUL,
+  CCF.DENUMIRE,
+  CC.CODUL,
+  CC.DENUMIRE,
+  CC.FULL_CODE,
+  CC.ORDINE
+  
+  ORDER BY 
+  CC.ORDINE
+  ) B
