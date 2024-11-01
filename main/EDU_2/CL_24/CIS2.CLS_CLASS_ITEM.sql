24e2206ecff2d8143eff31ae70d92f55d1f0855f
eva57gr <eva57gradycdc@hotmail.com>
2024-11-01 14:20:43 +0200

    49

--- main/EDU_2/CL_24/CIS2.CLS_CLASS_ITEM.sql
+++ main/EDU_2/CL_24/CIS2.CLS_CLASS_ITEM.sql
@@ -1,10 +1,27 @@
 
+ SELECT
+   ROWNUM NR_ROW,
+ ITEM_CODE,
+ ITEM_PARENT,
+ NAME,
+ SHOW_ORDER
+ FROM
 
-  SELECT *
+( 
+ SELECT 
+ ITEM_CODE,
+ ITEM_PARENT,
+ NAME,
+ SHOW_ORDER
         FROM CIS2.CLS_CLASS_ITEM
         
         WHERE
         
         CLASS_CODE = 'SPEC_2EDU' 
+        
+        
+        
         ORDER BY 
         SHOW_ORDER
+
+)
