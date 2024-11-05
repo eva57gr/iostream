07da4bc1ac979a3304300f57322ef778a00f3819
eva57gr <eva57gradycdc@hotmail.com>
2024-11-05 11:53:35 +0200

    49

---
+++ main/EDU_2/_ID_MDTABLE = 13915.sql
@@ -0,0 +1,14 @@
+SELECT  
+        
+        TRIM(RINDOUT) CODUL,
+        DENUMIRE DENUMIRE,
+        
+        ORDINE,
+        
+         RIND FULL_CODE
+
+FROM CIS2.MD_RIND_OUT
+
+WHERE
+ ID_MDTABLE = 13915
+ 
