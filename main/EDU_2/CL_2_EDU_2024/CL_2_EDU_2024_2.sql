3851fc13f9cf56c7b46cf042e6ed9381e3d4687d
eva57gr <eva57gradycdc@hotmail.com>
2024-11-06 08:39:46 +0200

    Create CL_2_EDU_2024_2.sql

---
+++ main/EDU_2/CL_2_EDU_2024/CL_2_EDU_2024_2.sql
@@ -0,0 +1,11 @@
+
+        SELECT 
+        CODUL, 
+        DENUMIRE, 
+        GRUPA, 
+        FULL_CODE, 
+        ORDINE
+        FROM USER_BANCU.VW_CL_SPEC_2EDU_24
+        
+        ORDER BY 
+        ORDINE
