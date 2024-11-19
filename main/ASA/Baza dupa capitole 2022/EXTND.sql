e620d49544e064eb595e9218022ae79f50354bfa
eva57gr <eva57gradycdc@hotmail.com>
2024-11-19 13:28:59 +0200

    64

---
+++ main/ASA/Baza dupa capitole 2022/EXTND.sql
@@ -0,0 +1,35 @@
+STATUT = '1'  AND  
+ 
+----capitol IN (1124,1128) AND capitol_vers=2000
+---- AND RIND_VERS = 2012
+--
+----
+---- (CAPITOL = 1124
+---- AND RIND_VERS <> 2000 )
+---- 
+---- OR 
+---- 
+---- (CAPITOL = 1125
+---- AND RIND_VERS = 2012 )
+---- 
+----  OR 
+---- 
+---- (CAPITOL = 1126
+---- AND RIND_VERS = 2012 )
+---- 
+----   OR 
+---- 
+---- (CAPITOL = 1127
+---- AND RIND_VERS = 2012 )
+---- OR 
+--  
+-- (CAPITOL = 1128
+-- 
+-- AND RIND_VERS <> 2000 
+-- )
+
+FORM = 64 AND ID_MD NOT IN (
+58733,58734,58735,58736,58737,58738,58739,58740,58741,58742,58743,58744,58746,58747,69961 
+)
+
+AND CAPITOL IN (1124)
