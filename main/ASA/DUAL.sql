ea85bcd50044fa9838fef7d8f6fa711fc3687e78
eva57gr <eva57gradycdc@hotmail.com>
2024-12-03 11:41:44 +0200

    2.6_step_15

--- main/ASA/DUAL.sql
+++ main/ASA/DUAL.sql
@@ -1,1 +1,5 @@
-SELECT 1 ORDINE, 'Total'NR_ROW,  0 AS MIN, 999999999 AS MAX  FROM DUAL      
+SELECT 1 ORDINE, 'Total'  NR_ROW,  0 AS MIN, 999999999 AS MAX  FROM DUAL UNION   
+SELECT 2 ORDINE, 'au activat pina la 3 luni' NR_ROW,  0 AS MIN, 3 AS MAX  FROM DUAL UNION
+SELECT 3 ORDINE, 'au activat de la 3 pina la 6 luni'  NR_ROW,  3 AS MIN, 6 AS MAX  FROM DUAL UNION
+SELECT 4 ORDINE, 'au activat de la 6 pina la 9 luni'  NR_ROW,  6 AS MIN, 9 AS MAX  FROM DUAL UNION
+SELECT 5 ORDINE, 'au activat de la 9 pina la 12 luni' NR_ROW,  9 AS MIN, 12 AS MAX  FROM DUAL      
