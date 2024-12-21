69f52d1ce7efe285da8fc95ba6f4d08d634ba92f
eva57gr <eva57gradycdc@hotmail.com>
2024-12-21 12:29:57 +0200

    5

--- main/RENIM_2025/RENIM_2025_5.sql
+++ main/RENIM_2025/RENIM_2025_5.sql
@@ -14,11 +14,11 @@
 --        L.CUIIO
 --        
         
-                      LEFT JOIN CIS.VW_CL_CUATM C ON C.CODUL =  L.CUATM
+                  --    LEFT JOIN CIS.VW_CL_CUATM C ON C.CODUL =  L.CUATM
 --                        
                     --    LEFT JOIN CIS2.VW_CL_CAEM2 C ON C.CODUL =  L.CAEM2
 --                        
---                     --   LEFT JOIN CIS.VW_CL_CFP C ON C.CODUL =  L.CFP 
+                        LEFT JOIN CIS.VW_CL_CFP C ON C.CODUL =  L.CFP 
                         WHERE 
                         
                         C.CODUL IS   NULL 
---
+++ main/RENIM_2025/RENIM_2025_6.sql
@@ -0,0 +1,51 @@
+ SELECT *
+        FROM  USER_BANCU.RENIM_NEW;
+        
+--        
+--        ORDER BY 
+--        CUATM DESC  -- 41042980 -   44-0022
+        ;
+       
+        UPDATE USER_BANCU.RENIM_NEW
+        SET
+         CUATM = CASE WHEN (LENGTH(CUATM) = 6 AND (SUBSTR(CUATM,1,1) = 1  OR SUBSTR(CUATM,1,1) = 3 )) THEN '0'||CUATM ELSE CUATM END
+     --    CUIIO_VERS = 2012;
+        -- CFOJ = '420'
+        --CUATM = '0'||REPLACE(CUATM,'O','0')
+--        ,
+--          CUATM  = '0'||CUATM
+        ;
+        
+        -----------------------------------------------------
+             SELECT CUIIO
+               FROM USER_BANCU.TRIM_2_24_AGRO
+               
+               ;
+        
+        
+        
+        
+      SELECT 
+      CUIIO,
+      '0'||REPLACE(CUATM,'O','0') CUATM_REPLACE,
+      CUATM              
+        FROM USER_BANCU.ADD_NEW_SU;
+        
+        
+        
+        
+        SELECT 
+        CUIIO,
+        COUNT (CUIIO ) AS CNT
+        
+        FROM USER_BANCU.RSF_1
+        
+        GROUP BY 
+        CUIIO
+        
+        HAVING 
+        COUNT (CUIIO ) > 1 
+        ORDER BY 
+        
+        COUNT (CUIIO )
+        
