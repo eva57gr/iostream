4a38b1e91ded4358b6ec2b29b5635dbbe0d182f4
eva57gr <eva57gradycdc@hotmail.com>
2024-12-21 12:07:40 +0200

    7

---
+++ main/RENIM_2025/RENIM_2025_5.sql
@@ -0,0 +1,30 @@
+
+ SELECT 
+ 
+        L.CUIIO,
+        L.CUIIO_VERS,
+        L.DENUMIRE,
+        L.CUATM,
+        L.CFP,
+        L.CFOJ,
+        L.CAEM2
+        FROM  USER_BANCU.RENIM_NEW L
+        
+--        ORDER BY
+--        L.CUIIO
+--        
+        
+                      LEFT JOIN CIS.VW_CL_CUATM C ON C.CODUL =  L.CUATM
+--                        
+                    --    LEFT JOIN CIS2.VW_CL_CAEM2 C ON C.CODUL =  L.CAEM2
+--                        
+--                     --   LEFT JOIN CIS.VW_CL_CFP C ON C.CODUL =  L.CFP 
+                        WHERE 
+                        
+                        C.CODUL IS   NULL 
+
+
+
+                
+
+;
