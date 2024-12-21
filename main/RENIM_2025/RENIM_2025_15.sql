7a0f7adfeb5b5bd1364f66885b8118d694e14c97
eva57gr <eva57gradycdc@hotmail.com>
2024-12-21 18:19:27 +0200

    Create RENIM_2025_15.sql

---
+++ main/RENIM_2025/RENIM_2025_15.sql
@@ -0,0 +1,36 @@
+DECLARE -- ====================================================================
+
+CURSOR C IS
+
+SELECT 
+  CUIIO,
+  
+  IDNO  
+
+--------------------------------
+------------------------------------
+FROM USER_BANCU.RENIM_NEW 
+            
+            --------------------------------
+            ;
+
+BEGIN -- ======================================================================
+FOR CR IN C
+LOOP
+UPDATE CIS2.RENIM SET
+
+--DENUMIRE = CR.DENUMIRE,
+--CUATM = CR.CUATM,
+--CFP = CR.CFP,
+--CFOJ = CR.CFOJ,
+--CAEM2 = CR.CAEM2,
+IDNO = CR.IDNO
+
+WHERE
+CUIIO = CR.CUIIO; 
+--AND 
+--CUIIO_VERS = CR.CUIIO_VERS;
+END LOOP;
+END;
+
+---------------------------
