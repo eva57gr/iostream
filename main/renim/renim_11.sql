4c614b1d61b21d936219d8a56dc3b737822cd801
eva57gr <eva57gradycdc@hotmail.com>
2024-12-23 14:39:07 +0200

    45

--- main/renim/renim_11.sql
+++ main/renim/renim_11.sql
@@ -1,15 +1,15 @@
 --IF CUATM  starts with 'O' then change it to zero, if not then do nothing
 SELECT 
-CUIIO,
-CUIIO_VERS,
-DENUMIRE,
-CUATM, -- String Oracle 
-CFP,
-CFOJ,
-COCM,
-CAEM2,
-CAEM,
-IDNO 
+CUIIO
+--CUIIO_VERS,
+--DENUMIRE,
+--CUATM, -- String Oracle 
+--CFP,
+--CFOJ,
+--COCM,
+--CAEM2,
+--CAEM,
+--IDNO 
 
 
 FROM USER_BANCU.IDNO
