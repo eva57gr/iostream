834048cad8f52b187e60de6161fba4a6e893cb7e
eva57gr <eva57gradycdc@hotmail.com>
2025-01-02 07:40:25 +0200

    Update RENIM_2025_11.sql

--- main/RENIM_2025/RENIM_2025_11.sql
+++ main/RENIM_2025/RENIM_2025_11.sql
@@ -10,17 +10,17 @@
 
 
 
---SELECT 
---        L.CUIIO,
---        2012 CUIIO_VERS,
---        4 FORM,
---        2000    FORM_VERS,
---        '1' STATUT
---        
---        FROM 
---
---
---(
+SELECT 
+        L.CUIIO,
+        2013 CUIIO_VERS,
+        27 FORM,
+        2000    FORM_VERS,
+        '1' STATUT
+        
+        FROM 
+
+
+(
 
 
 SELECT   
@@ -44,12 +44,12 @@ SELECT     R.CUIIO,
               FROM CIS2.FORM_CUIIO  FC
                    INNER JOIN (  SELECT CUIIO, MAX (CUIIO_VERS) CUIIO_VERS
                                    FROM CIS2.FORM_CUIIO
-                                  WHERE FORM IN (71) AND CUIIO_VERS <= 2014
+                                  WHERE FORM IN (27) AND CUIIO_VERS <= 2013
                                GROUP BY CUIIO) BB
                        ON (    BB.CUIIO = FC.CUIIO
                            AND BB.CUIIO_VERS = FC.CUIIO_VERS)
-             WHERE FC.FORM IN (71) AND FC.STATUT <> '3'
-             AND FC.FORM_VERS = 2011
+             WHERE FC.FORM IN (27) AND FC.STATUT <> '3'
+           --  AND FC.FORM_VERS = 2011
              
              
              ) FC
@@ -60,15 +60,15 @@ SELECT     R.CUIIO,
                
                SELECT CUIIO
                
-        FROM USER_BANCU.RENIM_NEW
+        FROM USER_BANCU.RENIM_24_F
         
         
                ) L ON L.CUIIO = R.CUIIO
                
                
                WHERE 
-               L.CUIIO IS   NOT  NULL  
---               ) L  
+               L.CUIIO IS     NULL  
+               ) L  
               
                
                
