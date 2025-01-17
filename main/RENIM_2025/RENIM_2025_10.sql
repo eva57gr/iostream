cbbe5489faf7143cff7e32668fd3d94189cd0fb0
eva57gr <eva57gradycdc@hotmail.com>
2025-01-17 14:48:47 +0200

    4

--- main/RENIM_2025/RENIM_2025_10.sql
+++ main/RENIM_2025/RENIM_2025_10.sql
@@ -1,51 +1,51 @@
--- INSERT INTO CIS2.RENIM (
--- 
--- 
--- CUIIO,
---    CUIIO_VERS,
---    DENUMIRE,
---    EDIT_USER,
---    STATUT,
---    CUATM,
---    CFP,
---    CFOJ,
---    COCM,
---    CAEM,
---    BUGET,
---    TIP,
---    PROD,
---    FOR_CUB,
---    GENMUZEE,
---    TIPMUZEE,
---    TIP_LOCAL,
---    TIP_INST,
---    CAEM2,
---    N85_NTL,
---    N85_NTIIP,
---    N85_NDIIP,
---    N85_NPDS,
---    N85_NRIIP,
---    N85_NSIIP,
---    GENMUZEE2,
---    NFI,
---    NTII,
---    NPDS,
---    ORGANE,
---    TIP_INV,
---    RENIM_PERS,
---    ORGANE_COND,
---    GEN_INSTITUTIE,
---    IDNO
---
--- 
--- )
---
+ INSERT INTO CIS2.RENIM (
+ 
+ 
+ CUIIO,
+    CUIIO_VERS,
+    DENUMIRE,
+    EDIT_USER,
+    STATUT,
+    CUATM,
+    CFP,
+    CFOJ,
+    COCM,
+    CAEM,
+    BUGET,
+    TIP,
+    PROD,
+    FOR_CUB,
+    GENMUZEE,
+    TIPMUZEE,
+    TIP_LOCAL,
+    TIP_INST,
+    CAEM2,
+    N85_NTL,
+    N85_NTIIP,
+    N85_NDIIP,
+    N85_NPDS,
+    N85_NRIIP,
+    N85_NSIIP,
+    GENMUZEE2,
+    NFI,
+    NTII,
+    NPDS,
+    ORGANE,
+    TIP_INV,
+    RENIM_PERS,
+    ORGANE_COND,
+    GEN_INSTITUTIE,
+    IDNO
+
+ 
+ )
+
 
 
 
 SELECT 
  CUIIO,
- CUIIO_VERS,
+ 2013 CUIIO_VERS,
  DENUMIRE,
  EDIT_USER,
  STATUT,
@@ -83,8 +83,8 @@ SELECT
 
                     FROM  -- USER_BANCU.VW_RENIM_2012_CIS2
                      
-                          USER_BANCU.VW_MAX_RENIM_CIS2
-                    
+                          --USER_BANCU.VW_MAX_RENIM_CIS2
+                          VW_MAX_RENIM_CIS2_2013
                     
                   --          USER_BANCU.VW_MAX_RENIM_TRIM_CIS2
                     
@@ -96,37 +96,8 @@ SELECT
                    
 CUIIO IN (
 
- SELECT FC.CUIIO
- --,
-   --                FC.CUIIO_VERS,
-     --              FC.FORM,
-       --            FC.FORM_VERS,
-         --          FC.STATUT
-                   FROM
-      (
-      SELECT FC.CUIIO,
-                   FC.CUIIO_VERS,
-                   FC.FORM,
-                   FC.FORM_VERS,
-                   FC.STATUT
-              FROM CIS2.FORM_CUIIO  FC
-                   INNER JOIN (  SELECT CUIIO, MAX (CUIIO_VERS) CUIIO_VERS
-                                   FROM CIS2.FORM_CUIIO
-                                  WHERE FORM IN (:pFORM) AND CUIIO_VERS <= :pPERIOADA
-                                  
-                               GROUP BY CUIIO) BB
-                       ON (    BB.CUIIO = FC.CUIIO
-                           AND BB.CUIIO_VERS = FC.CUIIO_VERS)
-             WHERE 
-             FC.FORM IN (:pFORM) AND FC.STATUT <> '3'
-             AND FC.FORM_VERS = 2011
-             
-             ) FC 
-             
-             
-             WHERE 
-          
-          FC.CUIIO_VERS <>  2014
+   SELECT CUIIO
+                FROM IDNO
              
 )
 
@@ -134,7 +105,7 @@ CUIIO IN (
         AND 
         
         
-        CUIIO_VERS  <>  2014 
+        CUIIO_VERS  <>  2013 
          
          
 
