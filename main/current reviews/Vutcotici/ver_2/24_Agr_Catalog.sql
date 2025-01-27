b2c94b16547edd0befd6c050a067379bf0980b74
eva57gr <eva57gradycdc@hotmail.com>
2025-01-27 16:26:31 +0200

    27

--- main/current reviews/Vutcotici/ver_2/24_Agr_Catalog.sql
+++ main/current reviews/Vutcotici/ver_2/24_Agr_Catalog.sql
@@ -1,10 +1,9 @@
 SELECT     
-
-           R.CUIIO,
-           R. CUIIO_VERS,
-           FC.FORM,
-          
-           R.CUATM
+     R.CUIIO,
+     R. CUIIO_VERS,
+     R.DENUMIRE,
+     R.CUATM,
+     R.IDNO
            
       FROM (
       
@@ -18,7 +17,7 @@ SELECT
               
                    INNER JOIN (  SELECT CUIIO, MAX (CUIIO_VERS) CUIIO_VERS
                                    FROM CIS2.FORM_CUIIO
-                                  WHERE FORM IN (:pFORM) AND CUIIO_VERS <= :pPERIOADA_LUNA  
+                                  WHERE FORM IN (:pFORM) AND CUIIO_VERS <= :pPERIOADA_LUNA
                                GROUP BY CUIIO) BB
                        ON (    BB.CUIIO = FC.CUIIO
                            AND BB.CUIIO_VERS = FC.CUIIO_VERS)
