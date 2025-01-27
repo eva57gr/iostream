914a5dec13547437d210485029b84efb8b93253e
eva57gr <eva57gradycdc@hotmail.com>
2025-01-27 13:54:30 +0200

    45

--- main/RENIM_2025/RENIM_2025_6.sql
+++ main/RENIM_2025/RENIM_2025_6.sql
@@ -1,5 +1,5 @@
  SELECT *
-        FROM  USER_BANCU.RENIM_5_CON;
+        FROM  USER_BANCU.IDNO;
         
 --        
 --        ORDER BY 
@@ -8,8 +8,8 @@
        
         UPDATE USER_BANCU.IDNO
         SET
-         CUATM = CASE WHEN (LENGTH(CUATM) = 6 AND (SUBSTR(CUATM,1,1) = 1  OR SUBSTR(CUATM,1,1) = 3 )) THEN '0'||CUATM ELSE CUATM END
-     --    CUIIO_VERS = 2012;
+        -- CUATM = CASE WHEN (LENGTH(CUATM) = 6 AND (SUBSTR(CUATM,1,1) = 1  OR SUBSTR(CUATM,1,1) = 3 )) THEN '0'||CUATM ELSE CUATM END
+         CUIIO_VERS = 2013;
         -- CFOJ = '420'
         --CUATM = '0'||REPLACE(CUATM,'O','0')
 --        ,
--- main/RENIM_2025/RENIM_2025_7.sql
+++ main/RENIM_2025/RENIM_2025_7.sql
@@ -39,13 +39,13 @@
 --
 -- 
 -- )
-
+--
 
 
 
 SELECT 
  CUIIO,
- 2013 CUIIO_VERS,
+  2013 CUIIO_VERS,
  DENUMIRE,
  EDIT_USER,
  STATUT,
@@ -83,9 +83,9 @@ SELECT
 
                     FROM   --USER_BANCU.VW_RENIM_2012_CIS2
                      
-                          USER_BANCU.VW_MAX_RENIM_CIS2
+                           USER_BANCU.VW_MAX_RENIM_CIS2
                     
-         --              VW_RENIM_2013_CIS2
+                    --   VW_RENIM_2013_CIS2
                            -- USER_BANCU.VW_MAX_RENIM_TRIM_CIS2
                     
                     WHERE 
@@ -96,8 +96,62 @@ SELECT
                    
 CUIIO IN (
 
- SELECT CUIIO
-        FROM  USER_BANCU.RENIM_5_CON
+SELECT FC.CUIIO
+                  
+              FROM
+              (          
+          
+          
+      SELECT FC.CUIIO,
+                   FC.CUIIO_VERS,
+                   FC.FORM,
+                   FC.FORM_VERS,
+                   FC.STATUT
+              FROM CIS2.FORM_CUIIO  FC
+                   INNER JOIN (  SELECT CUIIO, MAX (CUIIO_VERS) CUIIO_VERS
+                                   FROM CIS2.FORM_CUIIO
+                                  WHERE FORM IN (:pFORM) AND CUIIO_VERS <= :pPERIOADA
+                                  
+                               GROUP BY CUIIO) BB
+                       ON (    BB.CUIIO = FC.CUIIO
+                           AND BB.CUIIO_VERS = FC.CUIIO_VERS)
+             WHERE 
+             FC.FORM IN (:pFORM) AND FC.STATUT <> '3') FC 
+             
+             
+             
+             WHERE
+             
+             
+             FC.CUIIO_VERS <> 2013
+ )
+
+AND CUIIO_VERS <> 2013         
+
+ORDER BY 
+
+CUIIO;
+
+
+
+
+SELECT 
+ CUIIO
+ 
+ FROM    VW_RENIM_2013_CIS2
+ 
+                    
+                    WHERE 
+                  
+                  
+
+                    
+                   
+CUIIO IN (
+
+ SELECT
+ DISTINCT  CUIIO
+        FROM  USER_BANCU.IDNO
 
           )
 
@@ -105,10 +159,4 @@ CUIIO IN (
         AND 
         
         
-        CUIIO_VERS  <>  2013 
-         
-         
-
-ORDER BY 
-
-CUIIO
+        CUIIO_VERS  =  2013 
