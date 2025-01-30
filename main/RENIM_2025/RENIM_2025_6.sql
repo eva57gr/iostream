5046bee9211b6c65f7657777b453759202fec6de
eva57gr <eva57gradycdc@hotmail.com>
2025-01-30 08:28:00 +0200

    2

--- main/RENIM_2025/RENIM_2025_6.sql
+++ main/RENIM_2025/RENIM_2025_6.sql
@@ -1,15 +1,15 @@
  SELECT *
-        FROM  USER_BANCU.IDNO;
+        FROM  USER_BANCU.CUATM_TRIM_4;
         
 --        
 --        ORDER BY 
 --        CUATM DESC  -- 41042980 -   44-0022
         ;
        
-        UPDATE USER_BANCU.IDNO
+        UPDATE USER_BANCU.CUATM_TRIM_4
         SET
-        -- CUATM = CASE WHEN (LENGTH(CUATM) = 6 AND (SUBSTR(CUATM,1,1) = 1  OR SUBSTR(CUATM,1,1) = 3 )) THEN '0'||CUATM ELSE CUATM END
-         CUIIO_VERS = 2013;
+         CUATM = CASE WHEN (LENGTH(CUATM) = 6 AND (SUBSTR(CUATM,1,1) = 1  OR SUBSTR(CUATM,1,1) = 3 )) THEN '0'||CUATM ELSE CUATM END
+        -- CUIIO_VERS = 2013;
         -- CFOJ = '420'
         --CUATM = '0'||REPLACE(CUATM,'O','0')
 --        ,
