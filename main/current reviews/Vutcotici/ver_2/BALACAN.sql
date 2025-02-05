b0faaf3d4e75f37ab914e044a49523a86a72ed27
eva57gr <eva57gradycdc@hotmail.com>
2025-02-05 15:37:13 +0200

    Update BALACAN.sql

--- main/current reviews/Vutcotici/ver_2/BALACAN.sql
+++ main/current reviews/Vutcotici/ver_2/BALACAN.sql
@@ -1,6 +1,7 @@
-SELECT DISTINCT 
-D.CUIIO,
-R.DENUMIRE
+SELECT 
+DISTINCT 
+D.CUIIO
+--RR.DENUMIRE
 --R.IDNO
          
     FROM (
@@ -9,16 +10,22 @@ R.DENUMIRE
                           D.CUIIO_VERS,
                           D.CUATM,
                           D.FORM
-            FROM USER_EREPORTING.VW_DATA_ALL_PRIMIT D
+            FROM USER_EREPORTING.VW_DATA_ALL_PRIMIT D 
+                   --   INNER JOIN CIS.DATA_ALL   R ON R.CUIIO = D.CUIIO AND R.CUIIO_VERS = D.CUIIO_VERS
+                                    
+                                            -- INNER JOIN CIS.RENIM ON R.CUIIO = D.CUIIO AND R.CUIIO_VERS = D.CUIIO_VERS  
                  --INNER JOIN CIS2.VW_CL_CUATM C ON D.CUATM = C.CODUL
                  
            WHERE D.PERIOADA  = :pPERIOADA_LUNA   AND D.FORM IN (:pFORM)
-             AND D.ID_SCHEMA = 1
+             AND D.ID_SCHEMA = '2'
              
                  
            ) D
-         INNER JOIN CIS.RENIM  R ON R.CUIIO = D.CUIIO AND R.CUIIO_VERS = D.CUIIO_VERS
+          INNER JOIN CIS2.DATA_ALL   R ON R.CUIIO = D.CUIIO AND R.CUIIO_VERS = D.CUIIO_VERS
+       --   INNER JOIN CIS.RENIM         RR ON R.CUIIO = R.CUIIO AND RR.CUIIO_VERS = R.CUIIO_VERS
 GROUP BY 
-D.CUIIO,
-R.DENUMIRE
+D.CUIIO
+--,
+--RR.DENUMIRE
+--R.IDNO
 
