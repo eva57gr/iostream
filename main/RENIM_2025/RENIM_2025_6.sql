f6a73663301bf867e89cc2ea8c10c36bd6babbb6
eva57gr <eva57gradycdc@hotmail.com>
2025-02-06 13:49:26 +0200

    42-3

--- main/RENIM_2025/RENIM_2025_6.sql
+++ main/RENIM_2025/RENIM_2025_6.sql
@@ -1,24 +1,50 @@
  SELECT *
-        FROM  USER_BANCU.PRETIND;
+        FROM  USER_BANCU.ADD_NEW_SU_M3_2024;
         
 --        
 --        ORDER BY 
 --        CUATM DESC  -- 41042980 -   44-0022
         ;
-       
-        UPDATE USER_BANCU.PRETIND
+       --
+       --Add some more logic here.
+--If the CUATM column starts with 'O' then replace it with '0' if not then no
+        UPDATE USER_BANCU.ADD_NEW_SU_M3_2024
         SET
-         CUATM = CASE WHEN (LENGTH(CUATM) = 6 AND (SUBSTR(CUATM,1,1) = 1  OR SUBSTR(CUATM,1,1) = 3 )) THEN '0'||CUATM ELSE CUATM END
-        -- CUIIO_VERS = 479;
+         CUATM = CASE WHEN (LENGTH(CUATM) = 6 AND (SUBSTR(CUATM,1,1) = 1  OR SUBSTR(CUATM,1,1) = 3 )) THEN '0'||CUATM ELSE CUATM END;
+        -- CUIIO_VERS = 1064;
         -- CFOJ = '420'
         --CUATM = '0'||REPLACE(CUATM,'O','0')
 --        ,
 --          CUATM  = '0'||CUATM
         ;
         
+        
+        UPDATE USER_BANCU.AUTO_65
+SET CUATM = CASE 
+              WHEN LENGTH(CUATM) = 6 AND (SUBSTR(CUATM,1,1) = '1' OR SUBSTR(CUATM,1,1) = '3') 
+                THEN '0' || CUATM
+              WHEN SUBSTR(CUATM, 1, 1) = 'O' 
+                THEN '0' || SUBSTR(CUATM, 2) 
+              ELSE CUATM 
+            END;
+
+
+UPDATE USER_BANCU.ADD_NEW_SU_M3_2024
+SET CUATM = 
+    CASE 
+        -- Ruleaza prima verificare: înlocuie?te 'O' cu '0'
+        WHEN SUBSTR(CUATM, 1, 1) = 'O' THEN 
+            '0' || SUBSTR(CUATM, 2)  -- Concateneaza '0' cu restul ?irului
+        -- Apoi verifica daca trebuie adaugat '0' în fa?a
+        WHEN LENGTH(CUATM) = 6 AND SUBSTR(CUATM, 1, 1) IN ('1', '3') THEN 
+            '0' || CUATM  -- Prepand '0'
+        -- Daca nici o condi?ie nu este îndeplinita, pastreaza valoarea
+        ELSE 
+            CUATM
+    END;
         -----------------------------------------------------
-             SELECT CUIIO
-               FROM USER_BANCU.TRIM_2_24_AGRO
+             SELECT *
+               FROM USER_BANCU.ADD_NEW_SU_M3_2024
                
                ;
         
