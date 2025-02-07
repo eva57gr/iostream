f8e5dda6e294e97163c43f2f8e4554ba991c8abb
eva57gr <eva57gradycdc@hotmail.com>
2025-02-07 10:03:15 +0200

    m3

---
+++ main/RENIM_2025/update_renim.sql
@@ -0,0 +1,37 @@
+ SELECT 
+ 
+        L.CUIIO,
+        L.CUIIO_VERS,
+        L.DENUMIRE,
+        L.CUATM,
+        L.CFP,
+        L.CFOJ,
+        L.CAEM2
+       
+        
+        
+    
+        
+        FROM  ADD_NEW_SU_M3_2024 L 
+           LEFT JOIN CIS2.RENIM R 
+        
+        ON L.CUIIO =   R.CUIIO AND L.CUIIO_VERS = R.CUIIO_VERS
+        
+        WHERE
+        R.CUIIO IS NOT   NULL 
+        
+--       AND 
+--        TRIM(L.CUATM) <>  TRIM(R.CUATM)
+--        AND  
+--        TRIM(L.CAEM2) <>  TRIM(R.CAEM2)
+--        AND
+--        TRIM(L.CFP) = TRIM(R.CFP)
+--          AND
+--        TRIM(L.CFOJ) <>  TRIM(R.CFOJ)
+
+
+
+
+
+
+
