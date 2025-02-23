dae3fd021245e4630753966065be0ab27976dddd
eva57gr <eva57gradycdc@hotmail.com>
2025-02-23 11:39:24 +0200

    Update UPDATE_MD_CONTROL_CIS.sql

--- main/IND TS/UPDATE_MD_CONTROL_CIS.sql
+++ main/IND TS/UPDATE_MD_CONTROL_CIS.sql
@@ -25,7 +25,7 @@ SELECT
         FROM CIS.MD_CONTROL A
          WHERE
          1=1
-         AND A.FORM  = 3
+         AND A.FORM  = 6
         GROUP BY 
           A.CONTROL
           ORDER BY 
@@ -33,7 +33,7 @@ SELECT
         )  A  ON A.CONTROL = B.CONTROL AND A.CONTROL_VERS = B.CONTROL_VERS   
           WHERE
          1=1
-         AND B.FORM  = 3
+         AND B.FORM  = 6
          AND B.STATUT <> '3'
         ORDER BY 
           B.CONTROL ) L
