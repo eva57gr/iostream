26cb7fdb56b904db2e842c30a71583a64ca57747
eva57gr <eva57gradycdc@hotmail.com>
2025-02-25 18:27:41 +0200

    71

--- main/TIC/MD_RIND_1185_2014.sql
+++ main/TIC/MD_RIND_1185_2014.sql
@@ -9,7 +9,9 @@ SELECT
      
      D.CAPITOL  IN (1185)
      AND D.CAPITOL_VERS = 2014 
-     AND D.RIND IN ('511','512','520','531','532','533','541','542','543','560','570')   
+     AND D.RIND IN ('511','512','520','531','532','533','541','542','543','560','570')  
+     AND D.FORM_VERS = 2011
+      
 GROUP BY 
       D.RIND,
       D.RIND_VERS,
