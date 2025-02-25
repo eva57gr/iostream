d4fb0eea85e814e7f05bf214d9d2b21f9119b682
eva57gr <eva57gradycdc@hotmail.com>
2025-02-25 14:29:25 +0200

    54

--- main/pretind/Katalog/Check_CUATM_ADD_CUIIO_VERS.sql
+++ main/pretind/Katalog/Check_CUATM_ADD_CUIIO_VERS.sql
@@ -1,7 +1,7 @@
-      UPDATE  USER_BANCU.KAT_116
+      UPDATE  USER_BANCU.KAT_112
         SET
          CUATM = CASE WHEN (LENGTH(CUATM) = 6 AND (SUBSTR(CUATM,1,1) = 1  OR SUBSTR(CUATM,1,1) = 3 )) THEN '0'||CUATM ELSE CUATM END,
-         CUIIO_VERS = 2012
+         CUIIO_VERS = 2013
          
         --CUATM = '0'||REPLACE(CUATM,'O','0')
 --        ,
@@ -11,4 +11,4 @@
         
         
         SELECT *    
-        FROM USER_BANCU.KAT_116
+        FROM USER_BANCU.KAT_112;
---
+++ main/pretind/Katalog/update_form_cuiio_statut.sql
@@ -0,0 +1,76 @@
+UPDATE USER_EREPORTING.FORM_CUIIO_EXTRA
+
+SET STATUT = '3'
+
+--SELECT *
+--
+--
+--    FROM USER_EREPORTING.FORM_CUIIO_EXTRA
+    
+    WHERE
+    1=1 
+    
+    AND 
+    
+    CUIIO IN (
+    SELECT
+L.CUIIO
+FROM 
+(
+
+SELECT FC.CUIIO,
+                   FC.CUIIO_VERS,
+                   FC.FORM,
+                   FC.FORM_VERS,
+                   FC.STATUT,
+                   FC.ID_SCHEMA
+              FROM USER_EREPORTING.FORM_CUIIO_EXTRA  FC
+                   INNER JOIN (  SELECT CUIIO, MAX (CUIIO_VERS) CUIIO_VERS
+                                   FROM USER_EREPORTING.FORM_CUIIO_EXTRA
+                                  WHERE FORM IN (:pFORM) AND CUIIO_VERS <= :pPERIOADA
+                               GROUP BY CUIIO) BB
+                       ON (    BB.CUIIO = FC.CUIIO
+                           AND BB.CUIIO_VERS = FC.CUIIO_VERS)
+             WHERE FC.FORM IN (:pFORM)  AND FC.STATUT <> '3' 
+             
+             
+             
+             
+             ) L 
+             
+             
+             LEFT JOIN (
+
+
+SELECT
+  D.CUIIO,
+  D.CUIIO_VERS,
+  D.DENUMIRE,
+  D.CUATM,
+  -- CUATM_4,
+  --L.CODUL7 CUATM,  
+  D.CFP,
+  D.CFOJ,
+  D.COCM,
+  D.CAEM2,
+  D.CAEM,
+  D.IDNO    
+
+FROM USER_BANCU.KAT_112 D
+
+
+             ) R ON R.CUIIO = L.CUIIO 
+             
+             
+             WHERE 
+             R.CUIIO IS   NULL 
+    ) 
+    
+    AND FORM IN (112)
+    
+    AND CUIIO_VERS = 2013 
+    
+    AND FORM_VERS = 2000
+    AND STATUT = '1'
+    AND ID_SCHEMA = 4
+    
