b4728691302bb25c898fd2a581c508179f0461bf
eva57gr <eva57gradycdc@hotmail.com>
2025-03-11 09:03:36 +0200

    16.3

--- main/RENIM ADD  NEW KATALOG TEMPLATE/UPDATE_idno.sql
+++ main/RENIM ADD  NEW KATALOG TEMPLATE/UPDATE_idno.sql
@@ -2,53 +2,33 @@ DECLARE -- ====================================================================
 
 CURSOR C IS
 
---SELECT 
---  distinct 
---  
---  CUIIO,
---  CUIIO_VERS,
---  IDNO  
---
-----------------------------------
---------------------------------------
---FROM cis2.RENIM
---
---
---
---group by
---
---  CUIIO,
---  CUIIO_VERS,
---  IDNO  
---
---having 
---
---IDNO is not null 
---
---AND 
---(CUIIO_VERS >= 2011 and CUIIO_VERS < 2013)
---            
---            --------------------------------
-
-SELECT 
-  distinct CUIIO,
-  IDNO
-  
-  
-
---------------------------------
-------------------------------------
-FROM cis2.RENIM
+ SELECT 
+ 
+        L.CUIIO,
+        L.CUIIO_VERS,
+        L.DENUMIRE,
+        L.CUATM,
+        L.CFP,
+        L.CFOJ,
+        L.CAEM2,
+        RTRIM(TRIM(L.IDNO)) AS IDNO
+         FROM  USER_BANCU.AGRO_16 L
+        
+        WHERE
+        (L.IDNO IS NOT NULL
+        AND  
+        L.IDNO NOT IN ('???????'))
+        AND 
+        L.CFOJ IS NOT NULL
+        
+        AND 
+           
+        L.CFP IS NOT NULL
+        
+        ORDER BY        
+        IDNO
 
 
-
-GROUP BY
-CUIIO,
-IDNO 
-
-HAVING 
-
-IDNO IS NOT NULL
             ;
 
 BEGIN -- ======================================================================
@@ -58,15 +38,15 @@ UPDATE CIS2.RENIM SET
 
 --DENUMIRE = CR.DENUMIRE,
 --CUATM = CR.CUATM,
---CFP = CR.CFP,
---CFOJ = CR.CFOJ,
+CFP = CR.CFP,
+CFOJ = CR.CFOJ,
 --CAEM2 = CR.CAEM2,
 IDNO = CR.IDNO
 
 WHERE
-CUIIO = CR.CUIIO; 
---AND 
---CUIIO_VERS = CR.CUIIO_VERS;
+CUIIO = CR.CUIIO 
+AND 
+CUIIO_VERS = CR.CUIIO_VERS;
 END LOOP;
 END;
 
