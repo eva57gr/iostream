0e3e21c2f2986a7894428f7c8009c55c650c20e3
eva57gr <eva57gradycdc@hotmail.com>
2025-03-25 14:37:39 +0200

    36-01

--- main/RENIM_2025/RENIM_2025_7_CIs2.sql
+++ main/RENIM_2025/RENIM_2025_7_CIs2.sql
@@ -32,46 +32,46 @@
 
  
  
--- INSERT INTO CIS2.RENIM (
--- 
--- 
--- CUIIO,
---    CUIIO_VERS,
---    DENUMIRE,
---    EDIT_USER,
---    STATUT,
---    CUATM,
---    CFP,
---    CFOJ,
---    COCM,
---    CAEM,
---    BUGET,
---    TIP,
---    PROD,
---    FOR_CUB,
---    GENMUZEE,
---    TIPMUZEE,
---    TIP_LOCAL,
---    TIP_INST,
---    CAEM2,
---    N85_NTL,
---    N85_NTIIP,
---    N85_NDIIP,
---    N85_NPDS,
---    N85_NRIIP,
---    N85_NSIIP,
---    GENMUZEE2,
---    NFI,
---    NTII,
---    NPDS,
---    ORGANE,
---    TIP_INV,
---    RENIM_PERS,
---    ORGANE_COND,
---    GEN_INSTITUTIE,
---    IDNO
---)
--- 
+ INSERT INTO CIS2.RENIM (
+ 
+ 
+ CUIIO,
+    CUIIO_VERS,
+    DENUMIRE,
+    EDIT_USER,
+    STATUT,
+    CUATM,
+    CFP,
+    CFOJ,
+    COCM,
+    CAEM,
+    BUGET,
+    TIP,
+    PROD,
+    FOR_CUB,
+    GENMUZEE,
+    TIPMUZEE,
+    TIP_LOCAL,
+    TIP_INST,
+    CAEM2,
+    N85_NTL,
+    N85_NTIIP,
+    N85_NDIIP,
+    N85_NPDS,
+    N85_NRIIP,
+    N85_NSIIP,
+    GENMUZEE2,
+    NFI,
+    NTII,
+    NPDS,
+    ORGANE,
+    TIP_INV,
+    RENIM_PERS,
+    ORGANE_COND,
+    GEN_INSTITUTIE,
+    IDNO
+)
+ 
 
 
 
@@ -134,7 +134,7 @@ SELECT
 CUIIO IN (
 
 SELECT CUIIO
-        FROM  USER_BANCU.IDNO
+        FROM  USER_BANCU.AGRO_16
         
 
 ----                   FC.CUIIO_VERS,
@@ -195,7 +195,7 @@ CUIIO IN (
 
  SELECT
  DISTINCT  CUIIO
-        FROM  USER_BANCU.IDNO
+        FROM  USER_BANCU.AGRO_16
 
           )
 
