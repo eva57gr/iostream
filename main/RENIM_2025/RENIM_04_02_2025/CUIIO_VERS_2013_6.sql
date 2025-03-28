0c1cb2d452cc315b379cc846ff4dfcb27c831f91
eva57gr <eva57gradycdc@hotmail.com>
2025-03-28 15:06:30 +0200

    44

---
+++ main/RENIM_2025/RENIM_04_02_2025/CUIIO_VERS_2013_6.sql
@@ -0,0 +1,24 @@
+UPDATE USER_BANCU.ADD_NEW_SU_M3_2024
+SET CUATM = 
+    CASE 
+        -- Ruleaza prima verificare: înlocuie?te 'O' cu '0'
+        WHEN SUBSTR(CUATM, 1, 1) = 'O' THEN 
+            '0' || SUBSTR(CUATM, 2)  -- Concateneaza '0' cu restul ?irului
+        -- Apoi verifica daca trebuie adaugat '0' în fa?a
+        WHEN LENGTH(CUATM) = 6 AND SUBSTR(CUATM, 1, 1) IN ('1', '3') THEN 
+            '0' || CUATM  -- Prepand '0'
+        -- Daca nici o condi?ie nu este îndeplinita, pastreaza valoarea
+        ELSE 
+            CUATM
+    END;
+    
+    
+            UPDATE USER_BANCU.ADD_NEW_SU_M3_2024
+        SET
+         CUATM = CASE WHEN (LENGTH(CUATM) = 6 AND (SUBSTR(CUATM,1,1) = 1  OR SUBSTR(CUATM,1,1) = 3 )) THEN '0'||CUATM ELSE CUATM END,
+         CUIIO_VERS = 1064;
+        -- CFOJ = '420'
+        --CUATM = '0'||REPLACE(CUATM,'O','0')
+--        ,
+--          CUATM  = '0'||CUATM
+        ;
