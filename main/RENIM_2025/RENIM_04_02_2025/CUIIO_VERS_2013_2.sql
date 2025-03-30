8200683e0a66c58343373bc6a610b18c656ce6c9
eva57gr <eva57gradycdc@hotmail.com>
2025-03-30 11:43:04 +0300

    Update CUIIO_VERS_2013_2.sql

--- main/RENIM_2025/RENIM_04_02_2025/CUIIO_VERS_2013_2.sql
+++ main/RENIM_2025/RENIM_04_02_2025/CUIIO_VERS_2013_2.sql
@@ -1,5 +1,5 @@
-UPDATE CIS2.FORM_CUIIO 
-SET STATUT = '3'
+--UPDATE CIS2.FORM_CUIIO 
+--SET STATUT = '3'
 
 --SELECT *  ----------------
 ----
@@ -22,14 +22,14 @@ SELECT FC.CUIIO
               FROM CIS2.FORM_CUIIO  FC
                    INNER JOIN (  SELECT CUIIO, MAX (CUIIO_VERS) CUIIO_VERS
                                    FROM CIS2.FORM_CUIIO
-                                  WHERE FORM IN (57) AND CUIIO_VERS <= 2013
+                                  WHERE FORM IN (44) AND CUIIO_VERS <= 2013
                                   
                                GROUP BY CUIIO) BB
                        ON (    BB.CUIIO = FC.CUIIO
                            AND BB.CUIIO_VERS = FC.CUIIO_VERS)
              WHERE 
-             FC.FORM IN (57) AND FC.STATUT <> '3'
-             AND FC.FORM_VERS = 2009
+             FC.FORM IN (44) AND FC.STATUT <> '3'
+             AND FC.FORM_VERS = 1004
              ) FC
              
              
@@ -38,7 +38,7 @@ SELECT FC.CUIIO
              FC.CUIIO IN (
             SELECT
                DISTINCT  CUIIO
-            FROM  USER_BANCU.IDNO
+            FROM  USER_BANCU.ADD_NEW_SU_M3_2024
 
           ) )
 
