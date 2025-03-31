da9bf2ba41bb2b0500dc0802b0e5bad1f182ae9e
eva57gr <eva57gradycdc@hotmail.com>
2025-03-31 07:35:04 +0300

    Update CIS_RENIM_4.sql

--- main/RENIM_2025/CIS_RENIM_2025/CIS_RENIM_4.sql
+++ main/RENIM_2025/CIS_RENIM_2025/CIS_RENIM_4.sql
@@ -13,25 +13,25 @@ SELECT     R.CUIIO,
                    FC.FORM,
                    FC.FORM_VERS,
                    FC.STATUT
-              FROM CIS.FORM_CUIIO  FC
+              FROM CIS2.FORM_CUIIO  FC
                    INNER JOIN (  SELECT CUIIO, MAX (CUIIO_VERS) CUIIO_VERS
-                                   FROM CIS.FORM_CUIIO
-                                  WHERE FORM IN (6) AND CUIIO_VERS <= 1064
+                                   FROM CIS2.FORM_CUIIO
+                                  WHERE FORM IN (44) AND CUIIO_VERS <= 1064
                                GROUP BY CUIIO) BB
                        ON (    BB.CUIIO = FC.CUIIO
                            AND BB.CUIIO_VERS = FC.CUIIO_VERS)
-             WHERE FC.FORM IN (6) 
+             WHERE FC.FORM IN (44) 
              AND FC.STATUT <> '3'
              
              ) FC
-           INNER JOIN CIS.RENIM R
+           INNER JOIN CIS2.RENIM R
                ON (R.CUIIO = FC.CUIIO AND R.CUIIO_VERS = FC.CUIIO_VERS) ) R 
                
                RIGHT    JOIN (
                
                SELECT CUIIO
                
-        FROM USER_BANCU.INVEST_2
+        FROM USER_BANCU.ADD_NEW_SU_M3_2024
                ) L ON L.CUIIO = R.CUIIO
                WHERE 
                R.CUIIO IS NULL               
