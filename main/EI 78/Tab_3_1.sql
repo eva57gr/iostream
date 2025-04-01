036bd753d94dbf2de9805239d97ea21892e5bc4b
eva57gr <eva57gradycdc@hotmail.com>
2025-04-01 11:27:20 +0300

    Update Tab_3_1.sql

--- main/EI 78/Tab_3_1.sql
+++ main/EI 78/Tab_3_1.sql
@@ -49,7 +49,7 @@ FROM
 
      FROM
      (
-
+--1
 SELECT 
      CD.NR_ROW   AS NR_ROW,
      CD.NR_ROW   AS ORDINE,
@@ -103,9 +103,10 @@ SELECT
     CD.NR_ROW,
     CD.NUME_ROW,
     CR.COL1
-    
+    --1
     
     UNION 
+    --2
     SELECT 
      CD.NR_ROW   AS NR_ROW,
      CD.NR_ROW   AS ORDINE,
@@ -160,8 +161,10 @@ SELECT
     CD.NUME_ROW,
     CR.COL1
     
-    
+    --2
     UNION 
+    
+    --3
     SELECT 
      CD.NR_ROW   AS NR_ROW,
      CD.NR_ROW   AS ORDINE,
@@ -218,61 +221,7 @@ SELECT
       
     
     
-    UNION 
-    SELECT 
-     CD.NR_ROW   AS NR_ROW,
-     CD.NR_ROW   AS ORDINE,
-     CD.NUME_ROW AS NUME_ROW,
-
-    
-    ROUND((SUM(CASE WHEN CT.FUlL_CODE LIKE '%'||CD.CUATM||';%' AND   MR.CAPITOL IN (405)  AND MR.RIND NOT IN ('1','-')  THEN CIS2.NVAL(D.COL4) ELSE 0 END))/1000,1) AS COL1,
-    ROUND(((SUM(CASE WHEN  CT.FUlL_CODE LIKE '%'||CD.CUATM||';%' AND  MR.CAPITOL IN (405)  AND MR.RIND NOT IN ('1','-')  THEN CIS2.NVAL(D.COL4) ELSE 0 END)   ) / CR.COL1 )/1000,1) AS COL2,
-    ROUND((SUM(CASE WHEN  CT.FUlL_CODE LIKE '%'||CD.CUATM||';%' AND  MR.CAPITOL IN (407)  AND MR.RIND NOT IN ('1','-')  THEN CIS2.NVAL(D.COL4) ELSE 0 END) )/1000,1) AS COL3,
-    ROUND(((SUM(CASE WHEN CT.FUlL_CODE LIKE '%'||CD.CUATM||';%' AND   MR.CAPITOL IN (407)  AND MR.RIND NOT IN ('1','-')  THEN CIS2.NVAL(D.COL4) ELSE 0 END) ) / CR.COL1)/1000,1) AS COL4
-     
-     
    
- 
-    FROM DATA_ALL D
-          INNER JOIN RENIM RN ON D.CUIIO = RN.CUIIO AND D.CUIIO_VERS = RN.CUIIO_VERS
-          INNER  JOIN VW_CL_CUATM CT ON RN.CUATM = CT.CODUL
-          INNER  JOIN MD_RIND MR ON D.ID_MD = MR.ID_MD
-         
-          CROSS JOIN  (
-           SELECT DENUMIRE AS NUME_ROW, RIND AS NR_ROW,RINDOUT AS CUATM FROM CIS2.MD_RIND_OUT
-             WHERE 
-            ID_MDTABLE = 8096) CD  
-            
-            CROSS JOIN (
-         
-             SELECT
-            SUM(D.COL1)  AS COL1            
-                  FROM DATA_ALL D
-                         
-                      
-                        WHERE
-                            (D.PERIOADA =1057+4) AND          
-                            
-                           D.ID_MD  = 44519                        
-                              
-                              
-                              ) CR 
-             
- WHERE 
-  (D.PERIOADA =1057+4) AND 
-  (D.FORM =:pFORM) AND
-  (D.FORM_VERS =:pFORM_VERS) AND 
- -- (:pID_MDTABLE =:pID_MDTABLE) AND
-  (CT.FULL_CODE LIKE '%'||:pCOD_CUATM||';%') AND
-  D.FORM IN (44) AND
-  MR.CAPITOL IN (405,407)
-   
-  
-  GROUP BY 
-    CD.NR_ROW,
-    CD.NUME_ROW,
-    CR.COL1
-    
     
     UNION 
     SELECT 
