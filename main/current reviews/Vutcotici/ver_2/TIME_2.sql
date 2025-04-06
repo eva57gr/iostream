c2e5163c549a7741d2ba169e5181928085a435ef
eva57gr <eva57gradycdc@hotmail.com>
2025-04-06 09:43:31 +0300

    Create TIME_2.sql

---
+++ main/current reviews/Vutcotici/ver_2/TIME_2.sql
@@ -0,0 +1,32 @@
+SELECT C.CODUL,
+         C.FULL_CODE,
+         
+ 
+ NULL AS   COL1,
+ NULL AS   COL2,
+ COUNT (DISTINCT CASE WHEN D.FORM  = :pFORM THEN  D.CUIIO END)  AS  COL3,
+  NULL AS COL4,
+  NULL AS COL5
+
+
+
+
+         
+         
+    FROM (
+    
+    SELECT DISTINCT D.CUIIO,
+                          D.CUIIO_VERS,
+                          D.CUATM,
+                          D.FORM
+            FROM USER_EREPORTING.VW_DATA_ALL_PRIMIT D
+                 INNER JOIN CIS.VW_CL_CUATM C ON D.CUATM = C.CODUL
+                 
+           WHERE D.PERIOADA  = :pPERIOADA_LUNA   AND D.FORM IN (:pFORM)
+             AND D.ID_SCHEMA = 1
+             
+              AND (D.DATA_REG  > TO_DATE('2/01/2025 00:00:00', 'MM/DD/YYYY HH24:MI:SS') AND D.DATA_REG  <   TO_DATE('2/31/2025 23:59:59', 'MM/DD/YYYY HH24:MI:SS'))
+                 
+           ) D
+         INNER JOIN CIS2.VW_CL_CUATM C ON C.CODUL = D.CUATM
+GROUP BY C.CODUL, C.FULL_CODE
