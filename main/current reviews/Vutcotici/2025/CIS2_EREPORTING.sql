7126e42a8744d3b2af76fbcb184e56e81ff7f4ca
eva57gr <eva57gradycdc@hotmail.com>
2025-04-23 17:38:17 +0300

    44

---
+++ main/current reviews/Vutcotici/2025/CIS2_EREPORTING.sql
@@ -0,0 +1,76 @@
+ SELECT
+    DISTINCT 
+    D.CUIIO,
+     D.CUIIO_VERS,
+     D.CUATM,
+    D.FORM,
+    D.COL1
+    
+    FROM 
+
+    (
+    SELECT DISTINCT D.CUIIO,
+                         D.CUIIO_VERS,
+                          D.CUATM,
+                          D.FORM,
+                          'CIS2' AS COL1
+                          
+            FROM CIS2.VW_DATA_ALL D
+            
+                 
+           WHERE D.PERIOADA = :pPERIOADA AND D.FORM IN (:pFORM)
+           
+           AND D.CUIIO NOT IN (
+            SELECT DISTINCT D.CUIIO
+                          
+                          
+            FROM USER_EREPORTING.VW_DATA_ALL_PRIMIT D
+                                        INNER JOIN CIS2.RENIM R ON R.CUIIO = D.CUIIO AND R.CUIIO_VERS = D.CUIIO_VERS 
+                
+           WHERE D.PERIOADA  = :pPERIOADA   AND D.FORM IN (:pFORM)
+             AND D.ID_SCHEMA = '2'
+           
+           )
+           
+           
+           UNION 
+           
+            SELECT DISTINCT D.CUIIO,
+                          D.CUIIO_VERS,
+                          D.CUATM,
+                          D.FORM,
+                          'EREPORTING' AS COL1
+            FROM USER_EREPORTING.VW_DATA_ALL_PRIMIT D
+                INNER JOIN CIS2.RENIM R ON R.CUIIO = D.CUIIO AND R.CUIIO_VERS = D.CUIIO_VERS 
+           WHERE D.PERIOADA  = :pPERIOADA   AND D.FORM IN (:pFORM)
+             AND D.ID_SCHEMA = '2'
+             
+             ) D  LEFT JOIN (
+             
+              SELECT DISTINCT D.CUIIO,
+                          D.CUIIO_VERS,
+                          D.CUATM,
+                          D.FORM,
+                          'CIS2' AS COL1
+                          
+            FROM CIS2.VW_DATA_ALL D
+            
+                 
+           WHERE D.PERIOADA = :pPERIOADA AND D.FORM IN (:pFORM)
+             ) R ON D.CUIIO = R.CUIIO
+             
+             
+             WHERE
+             
+             R.CUIIO IS NOT NULL;
+      
+
+------------------------------------------------------------------------------       
+             
+            SELECT 
+            CUIIO,
+    CUIIO_VERS,
+    CUATM,
+    FORM,
+    COL1    
+            FROM  VW_36;
