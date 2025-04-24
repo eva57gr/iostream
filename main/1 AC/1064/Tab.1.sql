7bc72998d4a652aef25005b48073f734817eb4ec
eva57gr <eva57gradycdc@hotmail.com>
2025-04-24 15:00:03 +0300

    15

---
+++ main/1 AC/1064/Tab.1.sql
@@ -0,0 +1,132 @@
+--INSERT INTO TABLE_OUT 
+--(
+--  PERIOADA,
+--  FORM,
+--  FORM_VERS,
+--  ID_MDTABLE,
+--  COD_CUATM,
+--  NR_SECTIE,
+--  NUME_SECTIE,
+--  NR_SECTIE1,
+--  NUME_SECTIE1,
+--  NR_SECTIE2,
+--  NUME_SECTIE2,
+--  NR_ROW,
+--  ORDINE,
+--  DECIMAL_POS,
+--  NUME_ROW,  
+--  COL1, COL2, COL3, COL4
+--)
+
+
+--13047
+SELECT
+--  :pPERIOADA AS PERIOADA,
+--  :pFORM AS FORM,
+--  :pFORM_VERS AS FORM_VERS,
+--  :pID_MDTABLE AS ID_MDTABLE,
+--  :pCOD_CUATM AS COD_CUATM,
+--  '0' AS NR_SECTIE,
+--  '0' AS NUME_SECTIE,
+--  '0' AS NR_SECTIE1,
+--  '0' AS NUME_SECTIE1,
+--  '0' AS NR_SECTIE2,
+--  '0' AS NUME_SECTIE2,
+  L.RIND AS NR_ROW,
+  L.ORDINE AS ORDINE,
+  '0111' AS DECIMAL_POS,
+  L.DENUMIRE AS NUME_ROW,
+  R.COL1 AS COL1,
+  R.COL2 AS COL2,
+  NULL AS COL3,
+  NULL AS COL4
+FROM 
+(
+SELECT
+RIND,
+DENUMIRE,
+RIND_VERS,
+ORDINE 
+
+FROM CIS2.MD_RIND 
+WHERE
+CAPITOL IN (1000)
+AND CAPITOL_VERS = 1060
+
+UNION 
+
+SELECT
+'00'RIND,
+  'Autorizatii de construire eliberate pentru cladiri rezidentiale - total (r.01 + r.05)' AS DENUMIRE,
+   
+1060  RIND_VERS,
+ 0 AS ORDINE
+
+
+FROM DUAL 
+
+ORDER BY
+ORDINE  
+
+
+
+) L LEFT JOIN (
+
+SELECT
+  
+    R.RIND,
+    R.DENUMIRE AS DENUMIRE,
+    R.ORDINE,
+    SUM(D.COL1) AS COL1,
+    SUM(D.COL2) AS COL2
+  FROM
+    CIS2.VW_DATA_ALL D INNER JOIN
+    CIS2.MD_RIND R ON(D.ID_MD=R.ID_MD)
+  WHERE
+    D.FORM = 15 AND
+    D.CAPITOL = 1000 AND
+    D.PERIOADA BETWEEN FLOOR(:pPERIOADA/4)*4 AND :pPERIOADA
+   -- D.PERIOADA IN (:pPERIOADA)
+    AND (CIS2.D.CUATM_FULL LIKE '%'||:pCOD_CUATM||';%')
+    
+   -- AND D.CUIIO = 4496728
+    
+  GROUP BY
+    R.RIND,
+    R.DENUMIRE,
+    R.ORDINE
+    
+    
+    
+    UNION 
+    
+    SELECT
+    '00' AS RIND,
+    'Autorizatii de construire eliberate pentru cladiri rezidentiale - total (r.01 + r.05)' AS DENUMIRE,
+    0 AS ORDINE,
+    SUM(D.COL1) AS COL1,
+    SUM(D.COL2) AS COL2
+  FROM
+    CIS2.VW_DATA_ALL D
+  WHERE
+    D.FORM = 15 AND
+    D.CAPITOL = 1000 AND
+    D.RIND IN('01', '05')
+    AND D.PERIOADA BETWEEN FLOOR(:pPERIOADA/4)*4 AND :pPERIOADA
+    
+   -- AND  D.PERIOADA IN (:pPERIOADA)
+    AND (CIS2.D.CUATM_FULL LIKE '%'||:pCOD_CUATM||';%')
+    
+    
+ --   AND D.CUIIO = 4496728
+    
+    
+    ORDER BY
+    ORDINE
+    
+
+)  R  ON TO_CHAR(R.RIND) = TO_CHAR(L.RIND)
+
+
+ORDER BY
+L.ORDINE 
---
+++ main/1 AC/1064/Tab.1_1064_1.sql
@@ -0,0 +1,12 @@
+    SELECT
+      T.NR_ROW,
+      T.COL1 AS COL1,
+      T.COL2 AS COL2
+    FROM CIS2.TABLE_OUT T
+    WHERE
+      T.COD_CUATM IN (:pCOD_CUATM)  AND
+      T.FORM = :pFORM         AND
+      T.ID_MDTABLE = 13141   AND      
+      T.PERIOADA =(:pPERIOADA-4) 
+      --AND
+    
