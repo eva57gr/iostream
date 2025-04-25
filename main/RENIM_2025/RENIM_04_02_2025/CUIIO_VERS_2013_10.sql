bc8d2ac5d8334874f96b4896547d1b3efeee0413
eva57gr <eva57gradycdc@hotmail.com>
2025-04-25 14:07:50 +0300

    1

--- main/RENIM_2025/RENIM_04_02_2025/CUIIO_VERS_2013_10.sql
+++ main/RENIM_2025/RENIM_04_02_2025/CUIIO_VERS_2013_10.sql
@@ -1,43 +1,43 @@
- INSERT INTO CIS2.RENIM (
- 
- 
- CUIIO,
-    CUIIO_VERS,
-    DENUMIRE,
-    EDIT_USER,
-    STATUT,
-    CUATM,
-    CFP,
-    CFOJ,
-    COCM,
-    CAEM,
-    BUGET,
-    TIP,
-    PROD,
-    FOR_CUB,
-    GENMUZEE,
-    TIPMUZEE,
-    TIP_LOCAL,
-    TIP_INST,
-    CAEM2,
-    N85_NTL,
-    N85_NTIIP,
-    N85_NDIIP,
-    N85_NPDS,
-    N85_NRIIP,
-    N85_NSIIP,
-    GENMUZEE2,
-    NFI,
-    NTII,
-    NPDS,
-    ORGANE,
-    TIP_INV,
-    RENIM_PERS,
-    ORGANE_COND,
-    GEN_INSTITUTIE,
-    IDNO
-)
+-- INSERT INTO CIS2.RENIM (
 -- 
+-- 
+-- CUIIO,
+--    CUIIO_VERS,
+--    DENUMIRE,
+--    EDIT_USER,
+--    STATUT,
+--    CUATM,
+--    CFP,
+--    CFOJ,
+--    COCM,
+--    CAEM,
+--    BUGET,
+--    TIP,
+--    PROD,
+--    FOR_CUB,
+--    GENMUZEE,
+--    TIPMUZEE,
+--    TIP_LOCAL,
+--    TIP_INST,
+--    CAEM2,
+--    N85_NTL,
+--    N85_NTIIP,
+--    N85_NDIIP,
+--    N85_NPDS,
+--    N85_NRIIP,
+--    N85_NSIIP,
+--    GENMUZEE2,
+--    NFI,
+--    NTII,
+--    NPDS,
+--    ORGANE,
+--    TIP_INV,
+--    RENIM_PERS,
+--    ORGANE_COND,
+--    GEN_INSTITUTIE,
+--    IDNO
+--)
+---- 
 
 
 
@@ -88,7 +88,7 @@ CUIIO IN (
 
 
 SELECT CUIIO 
- FROM USER_BANCU.CFP_1064
+ FROM USER_BANCU.CUATM_CIS
 
 --SELECT FC.CUIIO
 --              FROM
