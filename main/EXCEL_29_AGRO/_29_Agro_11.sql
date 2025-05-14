8c83b104807f36656497fb589c8634e3067b2eb6
eva57gr <eva57gradycdc@hotmail.com>
2025-05-14 11:35:39 +0300

    45-1

---
+++ main/EXCEL_29_AGRO/_29_Agro_11.sql
@@ -0,0 +1,34 @@
+SELECT
+           DISTINCT R.CUIIO,
+           R.CUIIO_VERS,
+           R.DENUMIRE,
+           R.CUATM,
+           R.CFP,
+           R.CFOJ,
+           R.COCM,
+           R.CAEM2,
+           R.CAEM,
+           R.IDNO
+
+  
+FROM 
+  CIS2.VW_DATA_ALL D INNER JOIN CIS2.VW_CL_CUATM C ON C.CODUL = D.CUATM
+                INNER JOIN CIS2.RENIM  R  ON R.CUIIO = D.CUIIO AND R.CUIIO_VERS = D.CUIIO_VERS
+ 
+WHERE
+  D.PERIOADA IN (1064) AND 
+
+
+  D.FORM IN (45)    
+ 
+GROUP BY
+  R.CUIIO,
+           R.CUIIO_VERS,
+           R.DENUMIRE,
+           R.CUATM,
+           R.CFP,
+           R.CFOJ,
+           R.COCM,
+           R.CAEM2,
+           R.CAEM,
+           R.IDNO;
--- main/EXCEL_29_AGRO/_29_Agro_5.sql
+++ main/EXCEL_29_AGRO/_29_Agro_5.sql
@@ -29,8 +29,11 @@ NOCACHE;
 
 
 SELECT *
-  FROM USER_BANCU.KATALOG_45_1063_D; 
+  FROM USER_BANCU.KATALOG_45_1064_D; 
 
+
+DELETE
+  FROM USER_BANCU.KATALOG_45_1064_D; 
 
 INSERT INTO USER_BANCU.KATALOG_45_1064_D 
 (
@@ -84,20 +87,17 @@ CREATE OR REPLACE FORCE VIEW VW_45_AGRO_1064
  
  )
 AS 
-
-
-
 SELECT
            DISTINCT R.CUIIO,
-           R.CUIIO_VERS,
-           R.DENUMIRE,
-           R.CUATM,
-           R.CFP,
-           R.CFOJ,
-           R.COCM,
-           R.CAEM2,
-           R.CAEM,
-           R.IDNO
+           MAX(R.CUIIO_VERS) CUIIO_VERS,
+           MAX(R.DENUMIRE) DENUMIRE,
+           MAX(R.CUATM) CUATM,
+           MAX(R.CFP) CFP,
+           MAX(R.CFOJ) CFOJ,
+           MAX(R.COCM) COCM,
+           MAX(R.CAEM2) CAEM2,
+           MAX(R.CAEM) CAEM,
+           MAX(R.IDNO) IDNO 
 
   
 FROM 
@@ -111,15 +111,45 @@ WHERE
   D.FORM IN (45)    
  
 GROUP BY
-  R.CUIIO,
-           R.CUIIO_VERS,
-           R.DENUMIRE,
-           R.CUATM,
-           R.CFP,
-           R.CFOJ,
-           R.COCM,
-           R.CAEM2,
-           R.CAEM,
-           R.IDNO
- 
-  
+  R.CUIIO;
+
+
+--SELECT
+--           DISTINCT R.CUIIO,
+--           R.CUIIO_VERS,
+--           R.DENUMIRE,
+--           R.CUATM,
+--           R.CFP,
+--           R.CFOJ,
+--           R.COCM,
+--           R.CAEM2,
+--           R.CAEM,
+--           R.IDNO
+--
+--  
+--FROM 
+--  CIS2.VW_DATA_ALL D INNER JOIN CIS2.VW_CL_CUATM C ON C.CODUL = D.CUATM
+--                INNER JOIN CIS2.RENIM  R  ON R.CUIIO = D.CUIIO AND R.CUIIO_VERS = D.CUIIO_VERS
+-- 
+--WHERE
+--  D.PERIOADA IN (1064) AND 
+--
+--
+--  D.FORM IN (45)    
+-- 
+--GROUP BY
+--  R.CUIIO,
+--           R.CUIIO_VERS,
+--           R.DENUMIRE,
+--           R.CUATM,
+--           R.CFP,
+--           R.CFOJ,
+--           R.COCM,
+--           R.CAEM2,
+--           R.CAEM,
+--           R.IDNO;
+           
+           
+           
+           
+           
