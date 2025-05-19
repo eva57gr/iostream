88fe8dddcd9500d1b956f7f95f94bc355c5b51a7
eva57gr <eva57gradycdc@hotmail.com>
2025-05-19 08:40:47 +0300

    4-1

--- main/RENIM_2025/RENIM_04_02_2025/CUIIO_VERS_2013_12.sql
+++ main/RENIM_2025/RENIM_04_02_2025/CUIIO_VERS_2013_12.sql
@@ -9,32 +9,32 @@
 --
 
 
-SELECT * 
-
-
- FROM CIS2.FORM_CUIIO 
- 
- WHERE
- 
- CUIIO IN (
-
-
-
-SELECT 
-        L.CUIIO
+--SELECT * 
+--
+--
+-- FROM CIS2.FORM_CUIIO 
+-- 
+-- WHERE
+-- 
+-- CUIIO IN (
+--
+--
+--
+--SELECT 
+--        L.CUIIO
 --        2013 CUIIO_VERS,
 --        27 FORM,
 --        2000    FORM_VERS,
 --        '1' STATUT
-        
-        FROM 
-
-
-(
+--        
+--        FROM 
+--
+--
+--(
 
 
 SELECT   
-         R.CUIIO 
+         L.CUIIO 
          -- R_CUIIO,       
         -- L.CUIIO  CUIIO
          
@@ -54,11 +54,11 @@ SELECT     R.CUIIO,
               FROM CIS2.FORM_CUIIO  FC
                    INNER JOIN (  SELECT CUIIO, MAX (CUIIO_VERS) CUIIO_VERS
                                    FROM CIS2.FORM_CUIIO
-                                  WHERE FORM IN (27) AND CUIIO_VERS <= 2013
+                                  WHERE FORM IN (4) AND CUIIO_VERS <= 2013
                                GROUP BY CUIIO) BB
                        ON (    BB.CUIIO = FC.CUIIO
                            AND BB.CUIIO_VERS = FC.CUIIO_VERS)
-             WHERE FC.FORM IN (27) AND FC.STATUT <> '3'
+             WHERE FC.FORM IN (4) AND FC.STATUT <> '3'
            --  AND FC.FORM_VERS = 2011
              
              
@@ -66,22 +66,26 @@ SELECT     R.CUIIO,
            INNER JOIN CIS2.RENIM R
                ON (R.CUIIO = FC.CUIIO AND R.CUIIO_VERS = FC.CUIIO_VERS) ) R 
                
-               LEFT  JOIN (
+               RIGHT  JOIN (
                
                SELECT CUIIO
                
-        FROM USER_BANCU.RENIM_24_F
+        FROM USER_BANCU.TR_AUTO_2025
         
+
         
                ) L ON L.CUIIO = R.CUIIO
                
                
                WHERE 
-               L.CUIIO IS     NULL  
-               ) L )  
+               R.CUIIO IS  NOT    NULL  
+           --    )
+               
+
+-- L )  
               
                
                
-              AND CUIIO_VERS = 2013
-              
-              AND FORM = 27 
+--              AND CUIIO_VERS = 2013
+--              
+--              AND FORM = 27 
