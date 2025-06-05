f6af1b51e637e0ab8903ae7d78e36a3a038b1ee0
eva57gr <eva57gradycdc@hotmail.com>
2025-06-05 16:02:09 +0300

    43-5

--- main/RENIM_2025/RENIM_04_02_2025/CUIIO_VERS_2013_5.sql
+++ main/RENIM_2025/RENIM_04_02_2025/CUIIO_VERS_2013_5.sql
@@ -1,9 +1,9 @@
 --Create SQL code to delete duplicates in Oracle by column CUIIO - which is number
 SELECT *
-FROM USER_BANCU.TR_AUTO_2025
+FROM USER_BANCU.K_4_AGRO_96
 WHERE CUIIO IN (
     SELECT CUIIO
-    FROM USER_BANCU.TR_AUTO_2025
+    FROM USER_BANCU.K_4_AGRO_96
     GROUP BY CUIIO
     HAVING COUNT(*) > 1
 )
