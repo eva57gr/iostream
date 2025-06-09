9a7c4eb427c788575b8bddf0696780923fbf1f5b
eva57gr <eva57gradycdc@hotmail.com>
2025-06-09 13:07:39 +0300

    Update PE_CAPITOLE_.sql

--- main/_29_AGRO_/PE_CAPITOLE_.sql
+++ main/_29_AGRO_/PE_CAPITOLE_.sql
@@ -20,7 +20,7 @@
                         INNER JOIN CIS2.MD_CAPITOL MR ON MR.CAPITOL = D.CAPITOL AND MR.CAPITOL_VERS = D.CAPITOL_VERS  
             
                     WHERE 
-                    D.PERIOADA IN (1063)
+                    D.PERIOADA IN (1064)
                     AND D.FORM IN (45)
                     AND MR.DEN_SHORT IN ('Cap.I')
                     
@@ -35,6 +35,7 @@
                 MR.DEN_SHORT
                 
                 ORDER BY
+                R.CUATM,
                 D.CUIIO,
                 MR.DEN_SHORT,
                 D.RIND
