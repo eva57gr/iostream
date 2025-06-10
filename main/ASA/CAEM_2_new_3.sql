200e29d509df00df82e543dd93776e19ff120eb3
eva57gr <eva57gradycdc@hotmail.com>
2025-06-10 11:37:03 +0300

    64-112-111

---
+++ main/ASA/CAEM_2_new_3.sql
@@ -0,0 +1,10 @@
+SELECT 
+        SUBSTR(CODUL, 2, 4) AS COL3
+    FROM CIS2.VW_CL_CAEM2
+    WHERE 
+        PRIM IN ('1') AND 
+        (
+            SUBSTR(CODUL, 2, 2) IN ('47', '45') 
+         --   64-111	ASA - CAP.1 Daca sunt completate rd. 111  în CAP.4 trebuie sa fie un CAEM ?i anume G451, 4532, 454, 47
+
+        )
