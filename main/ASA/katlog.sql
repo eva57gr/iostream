acc59ff1aba2ce762d50492cface3e465da3c66a
eva57gr <eva57gradycdc@hotmail.com>
2025-06-14 06:47:40 +0300

    45

---
+++ main/ASA/katlog.sql
@@ -0,0 +1,25 @@
+SELECT 
+TRIM(R.CUIIO) CUIIO
+
+
+FROM USER_BANCU.ASA_2024 L RIGHT JOIN (
+SELECT FC.CUIIO,
+                   FC.CUIIO_VERS,
+                   FC.FORM,
+                   FC.FORM_VERS,
+                   FC.STATUT
+              FROM CIS2.FORM_CUIIO  FC
+                   INNER JOIN (  SELECT CUIIO, MAX (CUIIO_VERS) CUIIO_VERS
+                                   FROM CIS2.FORM_CUIIO
+                                  WHERE FORM IN (64) AND CUIIO_VERS <= 2013
+                                  
+                               GROUP BY CUIIO) BB
+                       ON (    BB.CUIIO = FC.CUIIO
+                           AND BB.CUIIO_VERS = FC.CUIIO_VERS)
+             WHERE 
+             FC.FORM IN (64) AND FC.STATUT <> '3'
+             
+) R ON R.CUIIO = L.CUIIO 
+
+WHERE
+L.CUIIO IS  NULL
