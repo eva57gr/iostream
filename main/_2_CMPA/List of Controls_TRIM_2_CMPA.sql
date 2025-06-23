22d933a40dfc75e7881ce5cb163a2422b0b2553e
eva57gr <eva57gradycdc@hotmail.com>
2025-06-23 15:33:18 +0300

    Update List of Controls_TRIM_2_CMPA.sql

--- main/_2_CMPA/List of Controls_TRIM_2_CMPA.sql
+++ main/_2_CMPA/List of Controls_TRIM_2_CMPA.sql
@@ -29,7 +29,7 @@ SELECT
         
          WHERE
          1=1
-         AND A.FORM  = 64
+         AND A.FORM  = 45
                
          GROUP BY 
           A.CONTROL
@@ -42,7 +42,7 @@ SELECT
         
          WHERE
          1=1
-         AND B.FORM  = 64
+         AND B.FORM  = 45
          AND B.STATUT <> '3'
         -- AND B.FORM_VERS = 2009
                
@@ -52,3 +52,16 @@ SELECT
           B.CONTROL ) L
           
           --Vitalie Bancu
+          
+          
+         
+
+
+
+--    rând.1490 col.2 = rând.1491+1492 pe col.2
+--    rând. 1800 col. 3 =  rând. 1620 col.3 (cap.I)+ rând. 6100 col.2(cap.Ia)- autosuma
+--    rând. 1800 col. 5 =  rând. 1620 col.5- autosuma
+--    Rând. 5000 col. 1   = rând. 5100 col. 1 + (rând. 7100 col. 1 + rând. 7200 col. 1 + rând. 7300 col. 1 + rând. 7400 col. 1 + rând. 7500 col. 1 din cap. II)
+--    Rând. 5100 col. 1   = rând. 2000 col. 1
+
+ 
