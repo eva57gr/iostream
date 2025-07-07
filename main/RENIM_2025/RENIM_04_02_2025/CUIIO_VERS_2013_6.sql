af27e8189955fb408a5fb63c3faafb8089d90b57
eva57gr <eva57gradycdc@hotmail.com>
2025-07-07 11:12:23 +0300

    26-3

--- main/RENIM_2025/RENIM_04_02_2025/CUIIO_VERS_2013_6.sql
+++ main/RENIM_2025/RENIM_04_02_2025/CUIIO_VERS_2013_6.sql
@@ -1,4 +1,4 @@
-UPDATE USER_BANCU.VINZARI_21 ----------------------
+UPDATE USER_BANCU.RENIM_2024 ----------------------
 SET CUATM = 
     CASE 
         -- Ruleaza prima verificare: înlocuie?te 'O' cu '0'
@@ -13,10 +13,10 @@ SET CUATM =
     END;
     
     
-            UPDATE USER_BANCU.VINZARI_21
+            UPDATE USER_BANCU.RENIM_2024
         SET
          CUATM = CASE WHEN (LENGTH(CUATM) = 6 AND (SUBSTR(CUATM,1,1) = 1  OR SUBSTR(CUATM,1,1) = 3 )) THEN '0'||CUATM ELSE CUATM END,
-         CUIIO_VERS = 1065;
+         CUIIO_VERS = 2013;
         -- CFOJ = '420'
         --CUATM = '0'||REPLACE(CUATM,'O','0')
 --        ,
@@ -24,4 +24,4 @@ SET CUATM =
         ;
         
         SELECT *
-        FROM USER_BANCU.K_4_AGRO_96;
+        FROM USER_BANCU.RENIM_2024;
