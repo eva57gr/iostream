7a4db5300c6ec0d2acf3c5d60b1c9320d9c3b75c
eva57gr <eva57gradycdc@hotmail.com>
2025-07-24 15:34:26 +0300

    15

---
+++ main/1 AC/1064/Tab.1_1065_SQL_2.sql
@@ -0,0 +1,39 @@
+SELECT 
+ T.NR_ROW,
+ SUM(T.COL1) AS COL1,
+ SUM(T.COL2) AS COL2
+FROM
+(
+     SELECT
+      T.NR_ROW,
+      T.COL1 AS COL1,
+      T.COL2 AS COL2
+    FROM CIS2.TABLE_OUT T
+    WHERE
+      T.COD_CUATM IN (:pCOD_CUATM)  AND
+      T.FORM = :pFORM         AND
+      T.ID_MDTABLE = 13754   AND      
+      T.PERIOADA = (:pPERIOADA-4)
+    UNION   
+   -- 13753
+--13766
+  
+    SELECT
+      T.NR_ROW,
+      T.COL1 AS COL1,
+      T.COL2 AS COL2
+    FROM CIS2.TABLE_OUT T
+    WHERE
+      T.COD_CUATM IN (:pCOD_CUATM)  AND
+      T.FORM = :pFORM         AND
+      T.ID_MDTABLE = 13049   AND      
+      T.PERIOADA = (:pPERIOADA-5) 
+      
+      )  T
+      
+      GROUP BY
+      
+     T.NR_ROW
+    
+    
+    
