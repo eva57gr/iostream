76ff0057f37121b1799a9cfca945acb47cb19aad
eva57gr <eva57gradycdc@hotmail.com>
2025-07-27 22:21:09 +0300

    6-1

--- main/RENIM_2025/CIS_RENIM_2025/New 1.sql
+++ main/RENIM_2025/CIS_RENIM_2025/New 1.sql
@@ -1,31 +1,31 @@
-INSERT INTO CIS.RENIM 
-
-(
-
-    CUIIO,
-    CUIIO_VERS,
-    DENUMIRE,
-    EDIT_USER,
-    STATUT,
-    CUATM,
-    CFP,
-    CFOJ,
-    COCM,
-    CAEM,
-    BUGET,
-    TIP,
-    PROD,
-    FOR_CUB,
-    ETAPA_PROD,
-    RENIM_PERS,
-    CAEM2
-    
-)
+--INSERT INTO CIS.RENIM 
+--
+--(
+--
+--    CUIIO,
+--    CUIIO_VERS,
+--    DENUMIRE,
+--    EDIT_USER,
+--    STATUT,
+--    CUATM,
+--    CFP,
+--    CFOJ,
+--    COCM,
+--    CAEM,
+--    BUGET,
+--    TIP,
+--    PROD,
+--    FOR_CUB,
+--    ETAPA_PROD,
+--    RENIM_PERS,
+--    CAEM2
+--    
+--)
 
 SELECT
 
     CUIIO,
-    1065 CUIIO_VERS, 
+    485 CUIIO_VERS, 
     DENUMIRE,
     EDIT_USER,
     STATUT,
@@ -43,8 +43,8 @@ SELECT
     CAEM2
     
     
-   FROM  USER_BANCU.VW_MAX_RENIM_TRIM_CIS
-               --      VW_MAX_RENIM_299_CIS  
+   FROM  --USER_BANCU.VW_MAX_RENIM_TRIM_CIS
+                     VW_MAX_RENIM_299_CIS  
    
    WHERE 
    
@@ -56,7 +56,7 @@ SELECT
     FROM CUATM_CIS
    )
    
-   AND CUIIO_VERS <> 1065
+   AND CUIIO_VERS <> 485
    
    
 --   SELECT *
