5859841f8998205495b73cf4fe0c7a5c2d69e090
eva57gr <eva57gradycdc@hotmail.com>
2025-07-29 07:24:50 +0300

    Update 4_.sql

--- main/EI 78/SQL 2025 07_28/4_.sql
+++ main/EI 78/SQL 2025 07_28/4_.sql
@@ -1,7 +1,7 @@
 SELECT 
 --     d.cuiio,   
      SUM(CASE WHEN  D.CAPITOL IN (405)   AND REPLACE(' '||CI.ITEM_PATH,';','; ') LIKE '% '||'9.2.2'||';%'
-      AND D.RIND NOT IN ('1','-')  THEN CIS2.NVAL(D.COL4) ELSE 0 END) AS COL1
+      AND D.RIND NOT IN ('1','-')  THEN CIS2.NVAL(D.COL4) ELSE 0 END) AS COL1 --2147040775
      
 
      FROM VW_DATA_ALL D
@@ -9,8 +9,7 @@ SELECT
          
          
                  
-         INNER JOIN VW_CLS_CLASS_ITEM CII ON (CII.CLASS_CODE IN ('CSPM2'))
-         AND REPLACE(' '||CI.ITEM_PATH,';','; ') LIKE '% '||TRIM(CII.ITEM_CODE)||';%'
+       
    WHERE 
   (D.PERIOADA =:pPERIOADA) AND 
   (D.FORM =:pFORM) AND
